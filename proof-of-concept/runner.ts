ea8bde23b15c78511439a495057026127c53f642
James Richford <jamesrichford@googlemail.com>
2016-06-09 23:10:46 +0100

    Include test cases in total count

--- proof-of-concept/runner.ts
+++ proof-of-concept/runner.ts
@@ -2,47 +2,77 @@ import * as Test from "./example-test";
 import "reflect-metadata";
 
 let testFixtureKeys = Object.keys(Test);
+let testFixtures: Array<any> = [];
 
+// CALCULATE TESTS TO RUN
 testFixtureKeys.forEach(testFixtureKey => {
+
+  let testFixture: any = {};
+  testFixtures.push(testFixture);
+
   // create an instance of the test fixture
-  let testFixture = new Test[testFixtureKey]();
+  testFixture.fixture = new Test[testFixtureKey]();
 
   // find all the tests on this test fixture
-  let testKeys = Reflect.getMetadata("alsatian:tests", testFixture);
+  let testKeys = Reflect.getMetadata("alsatian:tests", testFixture.fixture);
 
   if (testKeys.length === 0) {
     console.error("no tests found");
     return;
   }
 
-  console.log("1.." + testKeys.length);
+  testFixture.tests = [];
 
-  // run all tests on this test fixture
   testKeys.forEach((testKey: string) => {
+    let test: any = {};
+    testFixture.tests.push(test);
 
-    let testCases = Reflect.getMetadata("alsatian:testcases", testFixture, testKey);
+    test.key = testKey;
+    test.description = testKey;
+
+    let testCases = Reflect.getMetadata("alsatian:testcases", testFixture.fixture, testKey);
+    test.testCases = [];
+
+    if (!testCases) {
+      test.testCases.push([]);
+    }
+    else {
+      testCases.forEach((testCase: any) => {
+        test.testCases.push(testCase);
+      });
+    }
+  });
+});
+
+// RUN
+let totalTestCount = testFixtures.map(x => x.tests.map((y: any) => y.testCases.length)).reduce((a, b) => a + b).reduce((c: number, d: number) => c + d);
+console.log("1.." + totalTestCount);
+
+testFixtures.forEach(testFixture => {
+  // run all tests on this test fixture
+  testFixture.tests.forEach((test: any) => {
 
     // if there are no test cases then just run the test
-    if (!testCases) {
+    /*if (!testFixture.test) {
       try {
-        testFixture[testKey].call(this);
+        testFixture.fixture[testKey].call(this);
         console.log("ok");
       }
       catch(error) {
         console.log("not ok");
       }
-    }
+    }*/
     // otherwise pass all the test case arguments
-    else {
-      testCases.forEach((testCase: any) => {
+    //else {
+      test.testCases.forEach((testCase: any) => {
         try {
-          testFixture[testKey].apply(this, testCase.arguments);
+          testFixture.fixture[test.key].apply(this, testCase.arguments);
           console.log("ok");
         }
         catch(error) {
           console.log("not ok");
         }
       });
-    }
+    //}
   });
 });
