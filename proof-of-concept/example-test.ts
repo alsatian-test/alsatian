0f663e7bddb0aac52c67b247eca121e0fdba0f39
James Richford <jamesrichford@googlemail.com>
2016-06-11 15:07:14 +0100

    Add more matchers

--- proof-of-concept/example-test.ts
+++ proof-of-concept/example-test.ts
@@ -9,6 +9,7 @@ import { FocusTest } from "./focus-test-decorator";
 import { FocusTests } from "./focus-tests-decorator";
 import { Setup } from "./setup-decorator";
 import { Teardown } from "./teardown-decorator";
+import { MatchError } from "./match-error";
 
 @FocusTests
 export class ExampleTest extends TestFixture {
@@ -77,23 +78,66 @@ export class ExampleTest extends TestFixture {
     // console.log("you should see me");
   }
 
-  @Setup
-  private _doMeBefore() {
-    console.log("before");
+  @Test()
+  public equalTest() {
+    Expect(1).toEqual("1");
+    Expect(2).not.toEqual("1");
   }
 
-  @Setup
-  private _doMeBeforeAswell() {
-    console.log("before 2");
+  @Test()
+  public matchTest() {
+    Expect("something").toMatch(/thing/);
+    Expect("something").not.toMatch(/that/);
   }
 
-  @Teardown
-  private _doMeAfter() {
-    console.log("after");
+  @Test()
+  public definedTest() {
+    Expect(null).toBeDefined();
+    Expect(undefined).not.toBeDefined();
   }
 
-  @Teardown
-  private _doMeAfterAswell() {
-    console.log("after 2");
+  @Test()
+  public nullTest() {
+    Expect(null).toBeNull();
+    Expect(undefined).not.toBeNull();
+  }
+
+  @Test()
+  public truthyTest() {
+    Expect(1).toBeTruthy();
+    Expect(0).not.toBeTruthy();
+  }
+
+  @Test()
+  public containTest() {
+    Expect([1, 2, 3]).toContain(1);
+    Expect([1, 2, 3]).not.toContain(4);
+  }
+
+  @Test()
+  public lessThanTest() {
+    Expect(3).toBeLessThan(4);
+    Expect(4).not.toBeLessThan(3);
+  }
+
+  @Test()
+  public greaterThanTest() {
+    Expect(4).toBeGreaterThan(3);
+    Expect(3).not.toBeGreaterThan(4);
+  }
+
+  @Test()
+  public throwTest() {
+    Expect(() => { throw new Error(); }).toThrow();
+    Expect(() => { }).not.toThrow();
+  }
+
+  @Test()
+  @FocusTest
+  public throwErrorTest() {
+    Expect(() => { throw new MatchError(1, 2, true); }).toThrowError(MatchError, "Expected 1 to be 2.");
+    Expect(() => { throw new MatchError(1, 2, false) }).not.toThrowError(MatchError, "Expected 1 to be 2.");
+    Expect(() => { throw new Error("Expected 1 to be 2.") }).not.toThrowError(MatchError, "Expected 1 to be 2.");
+    Expect(() => { throw new Error() }).not.toThrowError(MatchError, "Expected 1 to be 2.");
   }
 }
--- proof-of-concept/expect.ts
+++ proof-of-concept/expect.ts
@@ -23,4 +23,82 @@ class Matcher {
       throw new MatchError(this._actualValue, expectedValue, this._shouldMatch);
     }
   }
+
+  public toEqual(expectedValue: any) {
+    if (expectedValue != this._actualValue === this._shouldMatch) {
+      throw new MatchError(this._actualValue, expectedValue, this._shouldMatch);
+    }
+  }
+
+  public toMatch(regex: any) {
+    if (!regex.test(this._actualValue) === this._shouldMatch) {
+      throw new MatchError(this._actualValue, regex, this._shouldMatch);
+    }
+  }
+
+  public toBeDefined() {
+    if (this._actualValue === undefined === this._shouldMatch) {
+      throw new MatchError(this._actualValue, undefined, this._shouldMatch);
+    }
+  }
+
+  public toBeNull() {
+    if (this._actualValue !== null === this._shouldMatch) {
+      throw new MatchError(this._actualValue, null, this._shouldMatch);
+    }
+  }
+
+  public toBeTruthy() {
+    if ((this._actualValue && !this._shouldMatch) || (!this._actualValue && this._shouldMatch)) {
+      throw new MatchError(this._actualValue, "truthy", this._shouldMatch);
+    }
+  }
+
+  public toContain(expectedContent: any) {
+    if (this._actualValue.indexOf(expectedContent) === -1 === this._shouldMatch) {
+      throw new MatchError(this._actualValue, expectedContent, this._shouldMatch);
+    }
+  }
+
+  public toBeLessThan(upperLimit: any) {
+    if (this._actualValue > upperLimit === this._shouldMatch) {
+      throw new MatchError(this._actualValue, upperLimit, this._shouldMatch);
+    }
+  }
+
+  public toBeGreaterThan(lowerLimit: any) {
+    if (this._actualValue < lowerLimit === this._shouldMatch) {
+      throw new MatchError(this._actualValue, lowerLimit, this._shouldMatch);
+    }
+  }
+
+  public toThrow() {
+    let threwError = false;
+    try {
+      this._actualValue();
+    }
+    catch(error) {
+      threwError = true;
+    }
+
+    if (!threwError === this._shouldMatch) {
+      throw new MatchError(this._actualValue, "throw error", this._shouldMatch);
+    }
+  }
+
+  public toThrowError(errorType: (...args: Array<any>) => Error, errorMessage: string) {
+    let threwRightError = false;
+    try {
+      this._actualValue();
+    }
+    catch(error) {
+      if (error instanceof errorType && error.message === errorMessage) {
+        threwRightError = true;
+      }
+    }
+
+    if (!threwRightError === this._shouldMatch) {
+      throw new MatchError(this._actualValue, "throw right error", this._shouldMatch);
+    }
+  }
 }
