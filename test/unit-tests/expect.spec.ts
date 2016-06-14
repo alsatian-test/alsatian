e18c223fbeaa71bb4b877ac283355e7eda4a90d3
jamesrichford <jamesrichford@googlemail.com>
2016-06-14 11:46:15 +0100

    First set of test cases

--- test/unit-tests/expect.spec.ts
+++ test/unit-tests/expect.spec.ts
@@ -8,4 +8,21 @@ export class ExpectTests {
 
       Expect(() => { expect.toBeNull() }).not.toThrow();
    }
+
+   @TestCase(undefined)
+   @TestCase(0)
+   @TestCase(42)
+   @TestCase(-42)
+   @TestCase("")
+   @TestCase("something")
+   @TestCase({})
+   @TestCase({ with: "something" })
+   @TestCase([])
+   @TestCase([ 1 ])
+   @TestCase([ 1, 2 ])
+   public nullShouldThrowErrorForNonNullValues(value: any) {
+      let expect = Expect(value);
+
+      Expect(() => { expect.toBeNull() }).toThrow();
+   }
 }
