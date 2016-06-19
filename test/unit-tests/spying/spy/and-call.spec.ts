a981398cb2b624e282a70b0d62cbf5c09c82fb30
James Richford <jamesrichford@googlemail.com>
2016-06-19 21:58:35 +0100

    Add spy.andCall tests

---
+++ test/unit-tests/spying/spy/and-call.spec.ts
@@ -0,0 +1,63 @@
+import { Spy } from "../../../../core/spying/spy";
+import { Expect, Test, TestCase, SpyOn } from "../../../../core/alsatian-core";
+
+export class AndCallTests {
+
+  @Test()
+  public originalFunctionNotCalledIfSpyFaked() {
+    let object = {
+      originalFunction: () => {}
+    };
+
+    SpyOn(object, "originalFunction");
+
+    let originalFunction = object.originalFunction;
+
+    let spy = new Spy(object.originalFunction, object);
+
+    spy.andCall(() => {});
+
+    spy.call([]);
+
+    Expect(originalFunction).not.toHaveBeenCalled();
+  }
+
+ @Test()
+ public originalFunctionNotCalledIfSpyNotFaked() {
+   let object = {
+     originalFunction: () => {}
+   };
+
+   SpyOn(object, "originalFunction");
+
+   let originalFunction = object.originalFunction;
+
+   let spy = new Spy(object.originalFunction, object);
+
+   spy.call([]);
+
+   Expect(originalFunction).toHaveBeenCalled();
+ }
+
+  @TestCase(() => {})
+  @TestCase(() => 1 + 1)
+  public fakeFunctionNotCalledIfSpyNotFaked(fakeFunction: Function) {
+    let object = {
+      originalFunction: () => {}
+    };
+
+    let fake = {
+      function: fakeFunction
+    }
+
+    SpyOn(fake, "function");
+
+    let originalFunction = object.originalFunction;
+
+    let spy = new Spy(object.originalFunction, object);
+
+    spy.call([]);
+
+    Expect(fake.function).toHaveBeenCalled();
+  }
+}
