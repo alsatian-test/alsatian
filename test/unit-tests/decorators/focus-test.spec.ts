0b521b4df8825c2faf7e1f5bdb61d31b1bdace65
James Richford <jamesrichford@googlemail.com>
2016-06-19 21:20:05 +0100

    Cover focus test decorator

---
+++ test/unit-tests/decorators/focus-test.spec.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { FocusTest as FocusTestDecorator } from "../../../core/decorators/focus-test-decorator";
+import { Expect, Test, TestCase } from "../../../core/alsatian-core";
+
+export class TestDecoratorTests {
+
+    @TestCase("key")
+    @TestCase("another key")
+    @TestCase("something-different")
+    public focusTestKeyMetaDataAddedToCorrectKey(key: string) {
+
+       let testFixture = {};
+
+       FocusTestDecorator(testFixture, key, null);
+
+       Expect(Reflect.getMetadata("alsatian:fccus", testFixture, key)).toBe(true);
+    }
+}
