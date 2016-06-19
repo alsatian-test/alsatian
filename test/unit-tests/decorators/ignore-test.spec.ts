1d8d62696ce6fe53357b33815b34d2baca8e2937
James Richford <jamesrichford@googlemail.com>
2016-06-19 21:36:57 +0100

    Add ignore test decorator tests

---
+++ test/unit-tests/decorators/ignore-test.spec.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { IgnoreTest as IgnoreTestDecorator } from "../../../core/decorators/ignore-test-decorator";
+import { Expect, Test, TestCase } from "../../../core/alsatian-core";
+
+export class TestDecoratorTests {
+
+    @TestCase("key")
+    @TestCase("another key")
+    @TestCase("something-different")
+    public ignoreTestKeyMetaDataAddedToCorrectKey(key: string) {
+
+       let testFixture = {};
+
+       IgnoreTestDecorator(testFixture, key, null);
+
+       Expect(Reflect.getMetadata("alsatian:ignore", testFixture, key)).toBe(true);
+    }
+}
