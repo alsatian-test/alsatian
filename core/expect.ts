6052fde77269cbdb6e0703d945c41bdc7ec3bef6
James Richford <jamesrichford@googlemail.com>
2016-06-28 20:55:49 +0100

    JSDocs added for expect

--- core/expect.ts
+++ core/expect.ts
@@ -11,30 +11,48 @@ import {
     FunctionCallMatchError
 } from "./errors/_namespace";
 
+/**
+ * Allows checking of test outcomes
+ * @param actualValue - the value or function under test
+ */
 export function Expect(actualValue: any) {
   return new Matcher(actualValue);
 }
 
+/**
+ * Gives functionality to ensure the outcome of a test is as expected
+ */
 export class Matcher {
 
   private _actualValue: any;
   private _shouldMatch: boolean = true;
 
+  public constructor(actualValue: any) {
+    this._actualValue = actualValue;
+  }
+
+  /**
+   * Any subsequent matcher function will be looking for the opposite criteria
+   */
   public get not(): Matcher {
     this._shouldMatch = !this._shouldMatch;
     return this;
   }
 
-  public constructor(actualValue: any) {
-    this._actualValue = actualValue;
-  }
-
+  /**
+   * Checks that a value is identical to another
+   * @param expectedValue - the value that will be used to match
+   */
   public toBe(expectedValue: any) {
     if (expectedValue !== this._actualValue === this._shouldMatch) {
       throw new ExactMatchError(this._actualValue, expectedValue, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a value is equal to another (for objects the function will check for deep equality)
+   * @param expectedValue - the value that will be used to match
+   */
   public toEqual(expectedValue: any) {
     if (expectedValue != this._actualValue === this._shouldMatch) {
 
@@ -79,48 +97,76 @@ export class Matcher {
      return true;
   }
 
+  /**
+   * Checks that a value conforms to a regular expression
+   * @param regex - the regular expression that the actual value should match
+   */
   public toMatch(regex: RegExp) {
     if (!regex.test(this._actualValue) === this._shouldMatch) {
       throw new RegexMatchError(this._actualValue, regex, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a value is not undefined
+   */
   public toBeDefined() {
     if (this._actualValue === undefined === this._shouldMatch) {
       throw new ExactMatchError(this._actualValue, undefined, !this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a value is null
+   */
   public toBeNull() {
     if (this._actualValue !== null === this._shouldMatch) {
       throw new ExactMatchError(this._actualValue, null, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a value is equivalent to boolean true
+   */
   public toBeTruthy() {
     if ((this._actualValue && !this._shouldMatch) || (!this._actualValue && this._shouldMatch)) {
       throw new TruthyMatchError(this._actualValue, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a string contains another string or an array contains a specific item
+   * @param expectedContent - the string or array item that the value should contain
+   */
   public toContain(expectedContent: any) {
     if (this._actualValue.indexOf(expectedContent) === -1 === this._shouldMatch) {
       throw new ContentsMatchError(this._actualValue, expectedContent, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a number is less than a given limit
+   * @param upperLimit - the number that the number under test should be less than
+   */
   public toBeLessThan(upperLimit: number) {
     if (this._actualValue < upperLimit !== this._shouldMatch) {
       throw new LessThanMatchError(this._actualValue, upperLimit, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a number is greater than a given limit
+   * @param lowerLimit - the number that the number under test should be greater than
+   */
   public toBeGreaterThan(lowerLimit: number) {
     if (this._actualValue > lowerLimit !== this._shouldMatch) {
       throw new GreaterThanMatchError(this._actualValue, lowerLimit, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a function throws an error when executed
+   */
   public toThrow() {
     let threwError = false;
     let actualError: Error;
@@ -138,6 +184,11 @@ export class Matcher {
     }
   }
 
+  /**
+   * Checks that a function throws a specific error
+   * @param errorType - the type of error that should be thrown
+   * @param errorMessage - the message that the error should have
+   */
   public toThrowError(errorType: new (...args: Array<any>) => Error, errorMessage: string) {
     let threwRightError = false;
     let actualError: Error;
@@ -158,12 +209,19 @@ export class Matcher {
     }
   }
 
+  /**
+   * Checks that a spy has been called
+   */
   public toHaveBeenCalled() {
     if (this._actualValue.calls.length === 0 === this._shouldMatch) {
       throw new FunctionCallMatchError(this._actualValue, this._shouldMatch);
     }
   }
 
+  /**
+   * Checks that a spy has been called with the specified arguments
+   * @param args - a list of arguments that the spy should have been called with
+   */
   public toHaveBeenCalledWith(...args: Array<any>) {
     if (this._actualValue.calls.filter((call: any) => call.args.filter((arg: any, index: number) => arg === args[index]) && call.args.length === args.length).length === 0 === this._shouldMatch) {
       throw new FunctionCallMatchError(this._actualValue, this._shouldMatch, args);
