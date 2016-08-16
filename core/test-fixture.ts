d6d47333732a44a150d841fb9d59003a938057e6
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-16 13:02:28 +0100

    Test Fixture class

--- core/test-fixture.ts
+++ core/test-fixture.ts
@@ -8,12 +8,28 @@ export class TestFixture implements ITestFixture {
     focussed: boolean;
     tests: Array<ITest>;
 
-    public addTest(test: ITest): void {
+    constructor () {
+        this.tests = [];
+    }
 
+    public addTest(test: ITest): void {
+        // if the test is already here, don't add it
+        if (this.tests.indexOf(test) !== -1) {
+            return;
+        }
+
+        this.tests.push(test);
     }
 
     public getTests(): Array<ITest> {
-        return [];
+        let anyTestsFocussed = this.tests.filter(t => t.focussed).length > 0;
+
+        // if there are no tests focussed, return them all
+        if (!anyTestsFocussed) {
+            return this.tests;
+        }
+
+        return this.tests.filter(t => t.focussed);
     }
 
 }
