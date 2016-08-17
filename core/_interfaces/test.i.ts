8c062cb314135f9ca84cb09f17b90c83c40e6d14
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-17 13:57:10 +0100

    Add ignoreReason

--- core/_interfaces/test.i.ts
+++ core/_interfaces/test.i.ts
@@ -2,7 +2,10 @@ import { ITestCase } from "./test-case.i";
 
 export interface ITest {
   isAsync: boolean;
+
   ignored: boolean;
+  ignoreReason?: string;
+
   focussed: boolean;
   timeout: number;
   key: string;
