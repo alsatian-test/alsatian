6c6e517af3e3509405d02a43b1ca21a98cb0dd98
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-17 13:56:52 +0100

    Remove IIGnoreTestInfo

--- core/decorators/ignore-test-decorator.ts
+++ core/decorators/ignore-test-decorator.ts
@@ -1,14 +1,8 @@
 import "reflect-metadata";
-import { IIgnoredTestInfo } from "../_interfaces/ignored-test-info.i";
 
 export function IgnoreTest(reason?: string) {
     return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
-        let testInfo: IIgnoredTestInfo = {
-            ignored: true,
-            reason: reason
-        };
-
         // mark test method as ignored
-        Reflect.defineMetadata("alsatian:ignore", testInfo, target, propertyKey);
+        Reflect.defineMetadata("alsatian:ignore", true, target, propertyKey);
     };
 }
