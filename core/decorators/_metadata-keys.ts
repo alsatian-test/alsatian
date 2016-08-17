d167d42d770d17310f5d8c6d34d752d9012cdb74
James Monger <jameskmonger@hotmail.co.uk>
2016-08-17 22:28:31 +0100

    Create metadata keys constants file

---
+++ core/decorators/_metadata-keys.ts
@@ -0,0 +1,19 @@
+const TEST_KEY = "alsatian:tests";
+const FOCUS_KEY = "alsatian:focus";
+const IGNORE_KEY = "alsatian:ignore";
+const IGNORE_REASON_KEY = "alsatian:ignore-reason";
+const SETUP_KEY = "alsatian:setup";
+const TEARDOWN_KEY = "alsatian:teardown";
+const TEST_CASES_KEY = "alsatian:testcases";
+const TIMEOUT_KEY = "alsatian:timeout";
+
+export {
+    TEST_KEY,
+    FOCUS_KEY,
+    IGNORE_KEY,
+    IGNORE_REASON_KEY,
+    SETUP_KEY,
+    TEARDOWN_KEY,
+    TEST_CASES_KEY,
+    TIMEOUT_KEY
+};
