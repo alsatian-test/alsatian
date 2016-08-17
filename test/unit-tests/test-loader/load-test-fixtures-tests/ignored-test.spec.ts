f05e93b8545fce37bb6ec2b0f6cb1b51242c5a3c
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-17 13:58:25 +0100

    Test loading ignore reason

--- test/unit-tests/test-loader/load-test-fixtures-tests/ignored-test.spec.ts
+++ test/unit-tests/test-loader/load-test-fixtures-tests/ignored-test.spec.ts
@@ -53,4 +53,31 @@ export class IgnoredTestTests {
 
       Expect(testLoader.loadTestFixture("test")[0].tests[0].ignored).toBe(true);
     }
+
+    @TestCase("some reason")
+    @TestCase("another reason")
+    @TestCase("last one, promise!")
+    public singleIgnoredTestWithReason(reason: string) {
+      let fileRequirer = new FileRequirer();
+
+      let testFixtureInstance = {
+        "ignoredTest": () => {}
+      };
+      let unignoredTest = {
+        key: "ignoredTest"
+      };
+      Reflect.defineMetadata("alsatian:tests", [ unignoredTest ], testFixtureInstance);
+      Reflect.defineMetadata("alsatian:ignore", true, testFixtureInstance, "ignoredTest");
+      Reflect.defineMetadata("alsatian:ignore-reason", reason, testFixtureInstance, "ignoredTest");
+
+      let testFixtureConstructor = () => testFixtureInstance;
+
+      let spy = SpyOn(fileRequirer, "require");
+      spy.andStub();
+      spy.andReturn(testFixtureConstructor);
+
+      let testLoader = new TestLoader(fileRequirer);
+
+      Expect(testLoader.loadTestFixture("test")[0].tests[0].ignoreReason).toBe(reason);
+    }
  }
