e8437b4008f264898380f0a71ecbfbb35923c635
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-18 18:40:39 +0100

    Implement test fixture ignoring :smile:

--- core/decorators/ignore-tests-decorator.ts
+++ core/decorators/ignore-tests-decorator.ts
@@ -1,9 +1,10 @@
 import "reflect-metadata";
-import { IGNORE } from "./_metadata-keys";
+import { IGNORE, IGNORE_REASON } from "./_metadata-keys";
 
 export function IgnoreTests(reason?: string) {
     return (constructor: Function) => {
         // mark test class as ignored
         Reflect.defineMetadata(IGNORE, true, constructor);
+        Reflect.defineMetadata(IGNORE_REASON, reason, constructor);
     };
 }
