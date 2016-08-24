125ce124f31097eece8f9bf2e15b469028958b70
jamesrichford <jamesrichford@googlemail.com>
2016-08-24 17:48:28 +0100

    Add namespace

---
+++ core/_running.ts
@@ -0,0 +1,9 @@
+import { TestRunner } from "./running/test-runner";
+import { TestPlan } from "./running/test-plan";
+import { TestItem } from "./running/test-item";
+
+export {
+    TestRunner,
+    TestPlan,
+    TestItem
+};
