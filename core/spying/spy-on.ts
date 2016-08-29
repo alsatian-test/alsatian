8943d4b9e612730455fa8ad372ac494eb3d72e72
James Richford <jamesrichford@googlemail.com>
2016-08-29 17:53:36 +0100

    throw error if trying to SpyOn non function

--- core/spying/spy-on.ts
+++ core/spying/spy-on.ts
@@ -1,14 +1,14 @@
 import {
-    RestorableSpy,
-    PropertySpy
+   RestorableSpy,
+   PropertySpy
 } from "../_spying";
 
 export function SpyOn(target: any, functionName: string): RestorableSpy {
 
-  if (target[functionName] instanceof Function) {
-    return new RestorableSpy(target, functionName);
-  }
-  else {
-    return new PropertySpy(target, functionName);
-  }
+   if (target[functionName] instanceof Function) {
+      return new RestorableSpy(target, functionName);
+   }
+   else {
+      throw new TypeError(`${functionName} is not a function.`);
+   }
 }
