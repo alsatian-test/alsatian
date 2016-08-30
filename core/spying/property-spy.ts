e052949cab7b781fd639b00949eb8a3b18337f1d
jamesrichford <jamesrichford@googlemail.com>
2016-08-30 17:45:59 +0100

    Record get calls

--- core/spying/property-spy.ts
+++ core/spying/property-spy.ts
@@ -1,21 +1,27 @@
+import { SpyCall } from "./spy-call";
+
 export class PropertySpy<PropertyType> {
 
   private _originialGetter: () => PropertyType;
   private _originialSetter: (value: PropertyType) => void;
   private _value: PropertyType;
+  private _getCalls: Array<SpyCall> = [];
 
-  public constructor(target: any, functionName: string) {
+  public constructor(target: any, propertyName: string) {
 
-     this._originialGetter = Object.getOwnPropertyDescriptor(target, functionName).get;
-     this._originialSetter = Object.getOwnPropertyDescriptor(target, functionName).set;
+     this._originialGetter = Object.getOwnPropertyDescriptor(target, propertyName).get;
+     this._originialSetter = Object.getOwnPropertyDescriptor(target, propertyName).set;
 
-     Object.defineProperty(target, functionName, {
+     Object.defineProperty(target, propertyName, {
        get: this._get,
        set: this._originialSetter
      });
+
+     target[propertyName].getCalls = this._getCalls;
   }
 
   private _get() {
+    this._getCalls.push(new SpyCall([]));
     return this._value;
   }
 
