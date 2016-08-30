2a912f2c047725b4236c2bea231f727ed361b024
jamesrichford <jamesrichford@googlemail.com>
2016-08-30 17:50:46 +0100

    auto indent property-spy.ts

--- core/spying/property-spy.ts
+++ core/spying/property-spy.ts
@@ -2,33 +2,33 @@ import { SpyCall } from "./spy-call";
 
 export class PropertySpy<PropertyType> {
 
-  private _originialGetter: () => PropertyType;
-  private _originialSetter: (value: PropertyType) => void;
-  private _value: PropertyType;
-  private _getCalls: Array<SpyCall> = [];
-  private _setCalls: Array<SpyCall> = [];
+   private _originialGetter: () => PropertyType;
+   private _originialSetter: (value: PropertyType) => void;
+   private _value: PropertyType;
+   private _getCalls: Array<SpyCall> = [];
+   private _setCalls: Array<SpyCall> = [];
 
-  public constructor(target: any, propertyName: string) {
+   public constructor(target: any, propertyName: string) {
 
-     this._originialGetter = Object.getOwnPropertyDescriptor(target, propertyName).get;
-     this._originialSetter = Object.getOwnPropertyDescriptor(target, propertyName).set;
+      this._originialGetter = Object.getOwnPropertyDescriptor(target, propertyName).get;
+      this._originialSetter = Object.getOwnPropertyDescriptor(target, propertyName).set;
 
-     Object.defineProperty(target, propertyName, {
-       get: this._get,
-       set: this._originialSetter
-     });
+      Object.defineProperty(target, propertyName, {
+         get: this._get,
+         set: this._originialSetter
+      });
 
-     target[propertyName].getCalls = this._getCalls;
-     target[propertyName].setCalls = this._setCalls;
-  }
+      target[propertyName].getCalls = this._getCalls;
+      target[propertyName].setCalls = this._setCalls;
+   }
 
-  private _get() {
-    this._getCalls.push(new SpyCall([]));
-    return this._value;
-  }
+   private _get() {
+      this._getCalls.push(new SpyCall([]));
+      return this._value;
+   }
 
-  private _set(value: PropertyType) {
-    this._getCalls.push(new SpyCall([ value ]));
-    this._value = value;
-  }
+   private _set(value: PropertyType) {
+      this._getCalls.push(new SpyCall([ value ]));
+      this._value = value;
+   }
 }
