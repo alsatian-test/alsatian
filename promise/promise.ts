33c8c101453d8bf32b5ecf980c2be208d3efaaee
James Richford <jamesrichford@googlemail.com>
2016-09-01 19:27:36 +0100

    make es6-like promise

---
+++ promise/promise.ts
@@ -0,0 +1,26 @@
+export class Promise<T> {
+
+   private _resolveCallback: (resolvedValue: T) => any = () => {};
+   private _rejectCallback: (error: Error) => any = () => {};
+
+   public constructor(asyncFunction: (resolve: (resolvedValue: T) => any, reject: (error: Error) => any) => any) {
+      setTimeout(() => {
+         try {
+            asyncFunction(this._resolveCallback, this._rejectCallback);
+         }
+         catch (error) {
+            this._rejectCallback(error);
+         }
+      });
+   }
+
+   public then(callback: (resolvedValue: T) => any) {
+      this._resolveCallback = callback;
+      return this;
+   }
+
+   public catch(callback: (error: Error) => any) {
+      promise.rejectCallback = callback;
+      return this;
+   }
+}
