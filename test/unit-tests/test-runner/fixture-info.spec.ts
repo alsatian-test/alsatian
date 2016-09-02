059d6712fceb7820f3e213c29f581e5c05a14856
jamesrichford <jamesrichford@googlemail.com>
2016-09-02 09:00:32 +0100

    refactor fixture-info tests to use es-6 like promise

--- test/unit-tests/test-runner/fixture-info.spec.ts
+++ test/unit-tests/test-runner/fixture-info.spec.ts
@@ -3,100 +3,107 @@ import { TestBuilder } from "../../builders/test-builder";
 import { TestCaseBuilder } from "../../builders/test-case-builder";
 import { TestFixtureBuilder } from "../../builders/test-fixture-builder";
 import { OutputStreamBuilder } from "../../builders/output-stream-builder";
-import { createPromise } from "../../../promise/create-promise";
+import { Promise } from "../../../promise/promise";
 
 export class FixtureInfoTests {
 
-    private static _getExpectedFixtureOutput(fixtureName: string): string {
-        return `# FIXTURE ${fixtureName}\n`;
-    }
+   private static _getExpectedFixtureOutput(fixtureName: string): string {
+      return `# FIXTURE ${fixtureName}\n`;
+   }
 
-    @AsyncTest()
-    @TestCase("SomeFixtureName")
-    @TestCase("AnotherFixture")
-    public outputsFixtureNameWithPassingTest(description: string) {
-        let writeCalls: Array<string> = [ ];
+   @AsyncTest()
+   @TestCase("SomeFixtureName")
+   @TestCase("AnotherFixture")
+   public outputsFixtureNameWithPassingTest(description: string) {
+      let writeCalls: Array<string> = [ ];
 
-        let outputStream = new OutputStreamBuilder().build();
-        SpyOn(outputStream, "write").andCall((s: string) => {
-            writeCalls.push(s);
-        });
+      let outputStream = new OutputStreamBuilder().build();
+      SpyOn(outputStream, "write").andCall((s: string) => {
+         writeCalls.push(s);
+      });
 
-        let output = new TestOutput(outputStream);
+      let output = new TestOutput(outputStream);
 
-        let testSet = <TestSet>{
-            testFixtures: []
-        };
+      let testSet = <TestSet>{
+         testFixtures: []
+      };
 
-        let test = new TestBuilder()
-            .withKey("test")
-            .addTestCase(new TestCaseBuilder().build())
-            .build();
+      let test = new TestBuilder()
+      .withKey("test")
+      .addTestCase(new TestCaseBuilder().build())
+      .build();
 
-        let testFixture = new TestFixtureBuilder()
-            .withFixture({ test: () => { }})
-            .withDescription(description)
-            .addTest(test)
-            .build();
+      let testFixture = new TestFixtureBuilder()
+      .withFixture({ test: () => { }})
+      .withDescription(description)
+      .addTest(test)
+      .build();
 
-        testSet.testFixtures.push(testFixture);
+      testSet.testFixtures.push(testFixture);
 
-        let resultPromise: any = createPromise();
 
-        let testRunner = new TestRunner(output);
+      return new Promise<void>((resolve, reject) => {
 
-        testRunner.run(testSet).then(() => {
+         let testRunner = new TestRunner(output);
+
+         testRunner.run(testSet).then(() => {
             // it should output version, then plan, then fixture, then test
             Expect(writeCalls[2]).toBe(FixtureInfoTests._getExpectedFixtureOutput(description));
             Expect(writeCalls[3]).toBe(`ok 1 ${test.description}\n`);
-            resultPromise.resolve();
-        });
+            resolve();
+         })
+         .catch((error: Error) => {
+            reject(error);
+         });
 
-        return resultPromise;
-    }
+      });
+   }
 
-    @AsyncTest()
-    @TestCase("SomeFixtureName")
-    @TestCase("AnotherFixture")
-    public outputsFixtureNameWithFailingTest(description: string) {
-        let writeCalls: Array<string> = [ ];
+   @AsyncTest()
+   @TestCase("SomeFixtureName")
+   @TestCase("AnotherFixture")
+   public outputsFixtureNameWithFailingTest(description: string) {
+      let writeCalls: Array<string> = [ ];
 
-        let outputStream = new OutputStreamBuilder().build();
-        SpyOn(outputStream, "write").andCall((s: string) => {
-            writeCalls.push(s);
-        });
+      let outputStream = new OutputStreamBuilder().build();
+      SpyOn(outputStream, "write").andCall((s: string) => {
+         writeCalls.push(s);
+      });
 
-        let output = new TestOutput(outputStream);
+      let output = new TestOutput(outputStream);
 
-        let testSet = <TestSet>{
-            testFixtures: []
-        };
+      let testSet = <TestSet>{
+         testFixtures: []
+      };
 
-        let test = new TestBuilder()
-            .withKey("test")
-            .addTestCase(new TestCaseBuilder().build())
-            .build();
+      let test = new TestBuilder()
+      .withKey("test")
+      .addTestCase(new TestCaseBuilder().build())
+      .build();
 
-        let testFixture = new TestFixtureBuilder()
-            .withFixture({ test: () => { throw new MatchError("nothing", "something", "expected nothing to be something."); }})
-            .addTest(test)
-            .withDescription(description)
-            .build();
+      let testFixture = new TestFixtureBuilder()
+      .withFixture({ test: () => { throw new MatchError("nothing", "something", "expected nothing to be something."); }})
+      .addTest(test)
+      .withDescription(description)
+      .build();
 
-        testSet.testFixtures.push(testFixture);
+      testSet.testFixtures.push(testFixture);
 
-        let resultPromise: any = createPromise();
+      return new Promise<void>((resolve, reject) => {
+         
+         let testRunner = new TestRunner(output);
 
-        let testRunner = new TestRunner(output);
-
-        testRunner.run(testSet).then(() => {
+         testRunner.run(testSet).then(() => {
             // it should output version, then plan, then fixture, then test
             Expect(writeCalls[2]).toBe(FixtureInfoTests._getExpectedFixtureOutput(description));
             Expect(writeCalls[3]).toBe(`not ok 1 ${test.description}\n`);
-            resultPromise.resolve();
-        });
+            resolve();
+         })
+         .catch((error: Error) => {
+            reject(error);
+         });
 
-        return resultPromise;
-    }
+      });
+   }
 
 }
