e42dce717111194f52cc73078ae43aa76ebbc385
James Richford <jamesrichford@googlemail.com>
2016-09-04 19:57:42 +0100

    reject tests

--- test/unit-tests/promise/promise.spec.ts
+++ test/unit-tests/promise/promise.spec.ts
@@ -1,6 +1,39 @@
 import { Promise } from "../../../promise/promise";
-import { Expect, Test, TestCase } from "../../../core/alsatian-core";
+import { Expect, AsyncTest, TestCase, SpyOn, FocusTests } from "../../../core/alsatian-core";
 
+@FocusTests
 export class PromiseTests {
 
+   @AsyncTest()
+   @TestCase(new TypeError("something wrong"))
+   @TestCase(new RangeError("everything else is awful"))
+   @TestCase(new Error("just bad!"))
+   public rejectCallsCatchWithGivenError(error: Error) {
+      return new Promise((resolve, reject) => {
+
+         const handler = {
+            catch: () => {}
+         };
+
+         SpyOn(handler, "catch");
+
+         new Promise((subResolve, subReject) => {
+            subReject(error);
+            Expect(handler.catch).toHaveBeenCalledWith(error);
+            resolve();
+         })
+         .catch(handler.catch);
+      });
+   }
+
+   @AsyncTest()
+   public rejectDoesNotThrowErrorIfCatchNotCalled() {
+      return new Promise((resolve, reject) => {
+
+         new Promise((subResolve, subReject) => {
+            Expect(() => subReject(new Error())).not.toThrow();
+            resolve();
+         });
+      });
+   }
 }
