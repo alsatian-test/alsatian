9f5e95fc8775bdefe2a90acd73b4d2fe4ab774a8
jamesrichford <jamesrichford@googlemail.com>
2016-09-07 14:04:05 +0100

    fix linting issue in ITestFixture

--- core/_interfaces/test-fixture.i.ts
+++ core/_interfaces/test-fixture.i.ts
@@ -1,14 +1,14 @@
 import { ITest } from "./test.i";
 
 export interface ITestFixture {
-  fixture: { [id: string]: (...args: Array<any>) => any };
+   fixture: { [id: string]: (...args: Array<any>) => any };
 
-  ignored: boolean;
-  ignoreReason: string;
+   ignored: boolean;
+   ignoreReason: string;
 
-  focussed: boolean;
-  tests: Array<ITest>;
-  
-  description: string;
-  addTest(test: ITest): void;
+   focussed: boolean;
+   tests: Array<ITest>;
+
+   description: string;
+   addTest(test: ITest): void;
 }
