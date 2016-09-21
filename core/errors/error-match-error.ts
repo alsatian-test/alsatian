84ee4a5f345c1d1cca0637ef6096aed03ce0c700
James Richford <jamesrichford@googlemail.com>
2016-09-21 19:21:28 +0100

    format ErrorMatchError

--- core/errors/error-match-error.ts
+++ core/errors/error-match-error.ts
@@ -2,43 +2,43 @@ import { MatchError } from "../_errors";
 
 export class ErrorMatchError extends MatchError {
 
-  public constructor(actualError: Error, shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error, expectedErrorMessage?: string) {
-    super(actualError, expectedErrorMessage, "");
+   public constructor(actualError: Error, shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error, expectedErrorMessage?: string) {
+      super(actualError, expectedErrorMessage, "");
 
-    if (expectedErrorType || expectedErrorMessage) {
-      this._setWrongSpecificErrorMessage(actualError, shouldMatch, expectedErrorType, expectedErrorMessage);
-    }
-    else {
-      if (shouldMatch) {
-        this._message = `Expected an error to be thrown but no errors were thown.`;
+      if (expectedErrorType || expectedErrorMessage) {
+         this._setWrongSpecificErrorMessage(actualError, shouldMatch, expectedErrorType, expectedErrorMessage);
       }
       else {
-        this._message = `Expected an error not to be thrown but an error was thown.`;
+         if (shouldMatch) {
+            this._message = `Expected an error to be thrown but no errors were thown.`;
+         }
+         else {
+            this._message = `Expected an error not to be thrown but an error was thown.`;
+         }
       }
-    }
-  }
+   }
 
-  private _setWrongSpecificErrorMessage(actualError: Error, shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error, expectedErrorMessage?: string) {
-    if (expectedErrorType === undefined || expectedErrorType === null || (expectedErrorMessage && actualError instanceof expectedErrorType && expectedErrorMessage !== actualError.message)) {
-      this._setWrongMessageMessage(shouldMatch, expectedErrorMessage);
-    }
-    else if (expectedErrorMessage === undefined || (actualError && !(actualError instanceof expectedErrorType) && expectedErrorMessage === actualError.message)) {
-      this._setWrongTypeMessage(actualError, shouldMatch, expectedErrorType);
-    }
-    else {
-      this._setWrongMessageAndTypeMessage(shouldMatch, expectedErrorType, expectedErrorMessage);
-    }
-  }
+   private _setWrongSpecificErrorMessage(actualError: Error, shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error, expectedErrorMessage?: string) {
+      if (expectedErrorType === undefined || expectedErrorType === null || (expectedErrorMessage && actualError instanceof expectedErrorType && expectedErrorMessage !== actualError.message)) {
+         this._setWrongMessageMessage(shouldMatch, expectedErrorMessage);
+      }
+      else if (expectedErrorMessage === undefined || (actualError && !(actualError instanceof expectedErrorType) && expectedErrorMessage === actualError.message)) {
+         this._setWrongTypeMessage(actualError, shouldMatch, expectedErrorType);
+      }
+      else {
+         this._setWrongMessageAndTypeMessage(shouldMatch, expectedErrorType, expectedErrorMessage);
+      }
+   }
 
-  private _setWrongMessageMessage(shouldMatch: boolean, expectedErrorMessage?: string) {
-       this._message = `Expected an error with message "${expectedErrorMessage}" to ${!shouldMatch ? "not " : ""}have been thrown, but it was${!shouldMatch ? "" : "n't"}.`;
-  }
+   private _setWrongMessageMessage(shouldMatch: boolean, expectedErrorMessage?: string) {
+      this._message = `Expected an error with message "${expectedErrorMessage}" to ${!shouldMatch ? "not " : ""}have been thrown, but it was${!shouldMatch ? "" : "n't"}.`;
+   }
 
-  private _setWrongMessageAndTypeMessage(shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error, expectedErrorMessage?: string) {
-     this._message = `Expected an error with message "${expectedErrorMessage}" and type ${(<any>expectedErrorType)["name"]} to ${!shouldMatch ? "not " : ""}have been thrown, but it was${!shouldMatch ? "" : "n't"}.`;
-  }
+   private _setWrongMessageAndTypeMessage(shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error, expectedErrorMessage?: string) {
+      this._message = `Expected an error with message "${expectedErrorMessage}" and type ${(<any>expectedErrorType)["name"]} to ${!shouldMatch ? "not " : ""}have been thrown, but it was${!shouldMatch ? "" : "n't"}.`;
+   }
 
-  private _setWrongTypeMessage(actualError: Error, shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error) {
-    this._message = `Expected an error of type ${(<any>expectedErrorType)["name"]} to ${!shouldMatch ? "not " : ""}have been thrown, but ${shouldMatch ? (<any>actualError)["name"] + " was thrown instead" : "it was"}.`;
-  }
+   private _setWrongTypeMessage(actualError: Error, shouldMatch: boolean, expectedErrorType?: new (...args: Array<any>) => Error) {
+      this._message = `Expected an error of type ${(<any>expectedErrorType)["name"]} to ${!shouldMatch ? "not " : ""}have been thrown, but ${shouldMatch ? (<any>actualError)["name"] + " was thrown instead" : "it was"}.`;
+   }
 }
