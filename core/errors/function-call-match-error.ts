c9422eec2a9f64606b5eb6702449421bcbfe74ee
jamesrichford <jamesrichford@googlemail.com>
2016-09-23 12:27:07 +0100

    ammend FunctionCallMatchError expected and actual values

--- core/errors/function-call-match-error.ts
+++ core/errors/function-call-match-error.ts
@@ -1,9 +1,12 @@
 import { MatchError } from "../_errors";
+import { FunctionSpy } from "../_spying";
 
 export class FunctionCallMatchError extends MatchError {
 
-  public constructor(actualValue: any, shouldMatch: boolean, args?: Array<any>) {
+  public constructor(actualValue: FunctionSpy, shouldMatch: boolean, args?: Array<any>) {
     // rework expected / actual values
-    super(`function was ${shouldMatch ? "not " : ""}called.`, `function ${!shouldMatch ? "not " : ""}to be called.`, `Expected function ${!shouldMatch ? "not " : ""}to be called${args ? " with [" + args.map(arg => JSON.stringify(arg)).join(", ") + "]" : ""}.`);
+    super(`function was ${shouldMatch && !(args && actualValue.calls.length) ? "not " : ""}called${args && actualValue.calls.length ? " with " + actualValue.calls.map(call => JSON.stringify(call.args)).join(", ") : ""}.`,
+          `function ${!shouldMatch ? "not " : ""}to be called${args ? " with " + JSON.stringify(args) : ""}.`,
+          `Expected function ${!shouldMatch ? "not " : ""}to be called${args ? " with [" + args.map(arg => JSON.stringify(arg)).join(", ") + "]" : ""}.`);
   }
 }
--- test/unit-tests/errors/function-call-match-error.spec.ts
+++ test/unit-tests/errors/function-call-match-error.spec.ts
@@ -92,4 +92,64 @@ export class FunctionCallMatchErrorTests {
 
       Expect(error.message).toBe("Expected function not to be called with [" + args.map(arg => JSON.stringify(arg)).join(", ") + "].");
    }
+
+   @TestCase([[]])
+   @TestCase([[], []])
+   @TestCase([[1], [2], [3]])
+   @TestCase([["something", "and", "another", "thing"]])
+   @TestCase([["this", "or"], ["that", "other", "thing"]])
+   public actualValueAndShouldMatchShouldBeSetToFunctionWasNotCalledWithArguments(actualArgumentsList: Array<Array<any>>) {
+      let fakeFunction: any = { calls: [ ] };
+
+      actualArgumentsList.forEach(actualArguments => {
+         fakeFunction.calls.push({ args: actualArguments });
+      });
+
+      let error = new FunctionCallMatchError(fakeFunction, true, []);
+
+      Expect(error.actualValue).toBe("function was called with " + actualArgumentsList.map(args => JSON.stringify(args)).join(", ") + ".");
+   }
+
+   @TestCase([[]])
+   @TestCase([[], []])
+   @TestCase([[1], [2], [3]])
+   @TestCase([["something", "and", "another", "thing"]])
+   @TestCase([["this", "or"], ["that", "other", "thing"]])
+   public actualValueAndShouldNotMatchShouldBeSetToFunctionWasCalledWithArguments(actualArgumentsList: Array<Array<any>>) {
+      let fakeFunction: any = { calls: [ ] };
+
+      actualArgumentsList.forEach(actualArguments => {
+         fakeFunction.calls.push({ args: actualArguments });
+      });
+
+      let error = new FunctionCallMatchError(fakeFunction, false, []);
+
+      Expect(error.actualValue).toBe("function was called with " + actualArgumentsList.map(args => JSON.stringify(args)).join(", ") + ".");
+   }
+
+   @TestCase([])
+   @TestCase([1])
+   @TestCase(["something"])
+   @TestCase([1, "or", 2, "other", "things"])
+   public expectedValueAndShouldMatchShouldBeSetToFunctionToBeCalledWithArguments(expectedArguments: Array<any>) {
+      let fakeFunction: any = { calls: [ ] };
+
+      let error = new FunctionCallMatchError(fakeFunction, true, expectedArguments);
+
+      Expect(error.expectedValue).toBe("function to be called with " + JSON.stringify(expectedArguments) + ".");
+   }
+
+   @TestCase([])
+   @TestCase([1])
+   @TestCase(["something"])
+   @TestCase([1, "or", 2, "other", "things"])
+   public expectedValueAndShouldNotMatchShouldBeSetToFunctionNotToBeCalledWithArguments(expectedArguments: Array<any>) {
+      let fakeFunction: any = { calls: [ ] };
+
+      fakeFunction.calls.push({ args: expectedArguments });
+
+      let error = new FunctionCallMatchError(fakeFunction, false, expectedArguments);
+
+      Expect(error.expectedValue).toBe("function not to be called with " + JSON.stringify(expectedArguments) + ".");
+   }
 }
