86a813285ba7871d832c6847fe3f2d3bf0a51bb4
jamesrichford <jamesrichford@googlemail.com>
2016-09-23 13:47:15 +0100

    Remove to dos

--- core/errors/function-call-match-error.ts
+++ core/errors/function-call-match-error.ts
@@ -4,7 +4,7 @@ import { FunctionSpy } from "../_spying";
 export class FunctionCallMatchError extends MatchError {
 
   public constructor(actualValue: FunctionSpy, shouldMatch: boolean, args?: Array<any>) {
-    // rework expected / actual values
+
     super(`function was ${shouldMatch && !(args && actualValue.calls.length) ? "not " : ""}called${args && actualValue.calls.length ? " with " + actualValue.calls.map(call => JSON.stringify(call.args)).join(", ") : ""}.`,
           `function ${!shouldMatch ? "not " : ""}to be called${args ? " with " + JSON.stringify(args) : ""}.`,
           `Expected function ${!shouldMatch ? "not " : ""}to be called${args ? " with [" + args.map(arg => JSON.stringify(arg)).join(", ") + "]" : ""}.`);
