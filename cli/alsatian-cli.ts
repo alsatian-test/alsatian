7b2cdeb34c3c183458202667d751d6f19793697e
James Monger <jameskmonger@hotmail.co.uk>
2016-09-24 13:35:41 +0100

    Alsatian fix

--- cli/alsatian-cli.ts
+++ cli/alsatian-cli.ts
@@ -4,6 +4,10 @@ import { createTestSet, TestRunner, TestOutput } from "../core/alsatian-core";
 import { CliTestRunner } from "./cli-test-runner";
 import { AlsatianCliOptions } from "./alsatian-cli-options";
 
+const {
+  TapBark
+} = require("tap-bark");
+
 // get all arguments from the user
 let userArguments = new AlsatianCliOptions(process.argv.slice(2));
 
@@ -11,7 +15,13 @@ let userArguments = new AlsatianCliOptions(process.argv.slice(2));
 let testSet = createTestSet();
 testSet.addTestsFromFiles(userArguments.fileGlobs);
 
-let output = new TestOutput(process.stdout);
+// create the tap bark reporter
+let reporter = TapBark.create().getPipeable();
+
+// pipe the reporter into stdout
+reporter.pipe(process.stdout);
+
+let output = new TestOutput(reporter);
 
 // create alsatian test runner
 let testRunner = new TestRunner(output);
@@ -19,3 +29,5 @@ let testRunner = new TestRunner(output);
 // run the test set
 let cliTestRunner = new CliTestRunner(testRunner);
 cliTestRunner.run(testSet, userArguments.timeout);
+
+//console.log (TapBark.create.toString());
