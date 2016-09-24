3647f21732a3018ba487c8c8f0358561612f6d2b
James Monger <jameskmonger@hotmail.co.uk>
2016-09-24 13:45:40 +0100

    add tap option tests

---
+++ test/unit-tests/cli/alsatian-cli-options/tap-option.spec.ts
@@ -0,0 +1,28 @@
+import { Expect, Test, TestCase } from "../../../../core/alsatian-core";
+import { AlsatianCliOptions } from "../../../../cli/alsatian-cli-options";
+import { DuplicateCliArgumentError } from "../../../../cli/errors/duplicate-cli-argument-error";
+
+export class TapOptionTests {
+
+   @Test()
+   public tapDefaultsToFalse() {
+      const options = new AlsatianCliOptions([]);
+
+      Expect(options.tap).toBe(false);
+   }
+
+   @Test()
+   public tapTrueIfCalled(timeout: string) {
+      const options = new AlsatianCliOptions([ "--tap" ]);
+
+      Expect(options.tap).toBe(true);
+   }
+
+   @Test()
+   public duplicateTapArgumentsThrowsError() {
+      Expect(() => {
+        new AlsatianCliOptions([ "--tap", "--tap" ])
+      }).toThrowError(DuplicateCliArgumentError, "Duplicate \"tap\" arguments were provided.");
+   }
+
+}
