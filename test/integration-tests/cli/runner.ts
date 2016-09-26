abda8a1dc346efb43df1301ceaf3f9314e491f31
James Richford <jamesrichford@googlemail.com>
2016-09-26 18:41:31 +0100

    start investigation as to why cli test not working

--- test/integration-tests/cli/runner.ts
+++ test/integration-tests/cli/runner.ts
@@ -1,7 +1,21 @@
 import * as child from "child_process";
+import * as path from "path";
 
-const p = child.spawn("alsatian", [ "../node/tests/to-be.spec.js" ]);
+try {
+const p = child.spawn("alsatian", [ path.join(__dirname, "../node/tests/**/*.spec.js") ]);
 
 p.stdin.on("data", (data: string) => {
    console.log(data);
 });
+
+p.stderr.on("data", (data: string) => {
+   console.log(data);
+});
+
+p.on("close", (data: string) => {
+   console.log("closed", data);
+});
+}
+catch (error) {
+   console.log(error);
+}
