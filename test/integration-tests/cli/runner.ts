d2a1b5290dc39ce070a2756ec8f1c9c9eb0df811
James Richford <jamesrichford@googlemail.com>
2016-09-26 18:34:24 +0100

    rename where looking for tests

--- test/integration-tests/cli/runner.ts
+++ test/integration-tests/cli/runner.ts
@@ -1,6 +1,6 @@
 import * as child from "child_process";
 
-const p = child.spawn("alsatian", [ "../tests/to-be.spec.ts" ]);
+const p = child.spawn("alsatian", [ "../node/tests/to-be.spec.js" ]);
 
 p.stdin.on("data", (data: string) => {
    console.log(data);
