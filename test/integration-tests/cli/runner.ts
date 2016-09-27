f49ba5fec4f5b16d789fa5ef1d23ff663509be14
James Richford <jamesrichford@googlemail.com>
2016-09-27 07:46:09 +0100

    annotate console logs

--- test/integration-tests/cli/runner.ts
+++ test/integration-tests/cli/runner.ts
@@ -7,17 +7,21 @@ try {
 const p = child.spawn("alsatian", [ path.join(__dirname, "../node/tests/**/*.spec.js") ]);
 
 p.stdin.on("data", (data: string) => {
-   console.log(data);
+   console.log("stdin", data);
 });
 
 p.stderr.on("data", (data: string) => {
-   console.log(data);
+   console.log("sterr", data);
 });
 
 p.on("close", (data: string) => {
-   console.log("closed", data);
+   console.log("close", data);
+});
+
+p.on("error", (data: string) => {
+   console.log("close", data);
 });
 }
 catch (error) {
-   console.log(error);
+   console.log("error", error);
 }
