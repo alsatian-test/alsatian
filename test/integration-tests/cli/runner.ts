2dc921bb4931d9b83d82c652a784db906cdc4116
James Richford <jamesrichford@googlemail.com>
2016-09-29 21:06:31 +0100

    compare console output to expected

--- test/integration-tests/cli/runner.ts
+++ test/integration-tests/cli/runner.ts
@@ -1,13 +1,29 @@
 import * as child from "child_process";
 import * as path from "path";
-import { Expect, Test } from "../../../core/alsatian-core";
+import { Promise } from "../../../promise/promise";
+import { Expect, AsyncTest } from "../../../core/alsatian-core";
+import * as FileSystem from "fs";
 
 export class CliIntegrationTests {
-   @Test()
+
+   @AsyncTest()
    public proofOfConcept() {
 
       const result = child.exec("alsatian ./test/integration-tests/node/tests/**/*.spec.js");
 
-      Expect(result.on("exit or something", () => {})).toBeDefined();
+      let consoleOutput = "";
+
+      result.stdout.on("data", (data) => consoleOutput += data);
+      result.stderr.on("data", (data) => consoleOutput += data);
+
+      let expectedOutput = FileSystem.readFileSync("./test/integration-tests/expected-output/proof-of-concept.txt").toString();
+
+      return new Promise((resolve, reject) => {
+         result.on("close", (code: number) => {
+            Expect(consoleOutput).toBe(expectedOutput.replace(/\r/g,""));
+
+            resolve();
+         });
+      });
    }
 }
