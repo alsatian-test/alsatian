88e613027ca5135e1dd24761d180494e71b745a9
James Richford <jamesrichford@googlemail.com>
2016-10-01 13:57:31 +0100

    test through TypeMatcher constructor

---
+++ test/unit-tests/spying/type-matcher/constructor.spec.ts
@@ -0,0 +1,21 @@
+import { TypeMatcher } from "../../../../core/spying/type-matcher";
+import { Expect, TestCase } from "../../../../core/alsatian-core";
+
+export class TypeMatcherConstructorTests {
+
+   @TestCase(null)
+   @TestCase(undefined)
+   public nullOrUndefinedTypesThrowError(type: any) {
+      Expect(() => new TypeMatcher(type)).toThrowError(TypeError, "type must not be null or undefined");
+   }
+
+   @TestCase(Number)
+   @TestCase(String)
+   @TestCase(Boolean)
+   @TestCase(Array)
+   @TestCase(Object)
+   @TestCase(Error)
+   public objectTypesDoNotThrow(type: new (...args: Array<any>) => Object) {
+      Expect(() => new TypeMatcher(type)).not.toThrow();
+   }
+}
