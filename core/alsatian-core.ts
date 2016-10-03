fb62a058f000fa321e11d499a190ba3b6024abe3
James Richford <jamesrichford@googlemail.com>
2016-10-03 07:30:02 +0100

    expose Any function

--- core/alsatian-core.ts
+++ core/alsatian-core.ts
@@ -22,6 +22,7 @@ import {
 } from "./_decorators";
 
 import {
+   Any,
    FunctionSpy,
    SpyOn,
    SpyOnProperty
@@ -47,6 +48,7 @@ import {
 } from "./_running";
 
 export {
+   Any,
    AsyncTest,
    createTestSet,
    Expect,
