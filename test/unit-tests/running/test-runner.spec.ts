55c9c46dc28500dff081a8ccef44abe77fdf0826
James Richford <jamesrichford@googlemail.com>
2016-10-15 13:12:33 +0100

    plan test-runner functional tests

---
+++ test/unit-tests/running/test-runner.spec.ts
@@ -0,0 +1,21 @@
+// single test item run success
+
+// timeout (normal)
+
+// timeout (diff)
+
+// error
+
+// two test item run success
+
+// first timeout (normal)
+
+// second timeout (normal)
+
+// first timeout (diff)
+
+// second timeout (diff)
+
+// first error
+
+// second error
