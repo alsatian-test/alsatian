0272ba51537fe1493e2e14224a9a890b4e742107
James Richford <jamesrichford@googlemail.com>
2016-11-02 07:44:20 +0000

    add namespace for matchers

---
+++ core/matchers/index.ts
@@ -0,0 +1,9 @@
+import { FunctionSpyMatcher } from "./function-spy-matcher";
+import { FunctionSpyCallCountMatcher } from "./function-spy-call-count-matcher";
+import { SpyCallCountType } from "./spy-call-count-type";
+
+export {
+   FunctionSpyMatcher,
+   FunctionSpyCallCountMatcher,
+   SpyCallCountType
+}
