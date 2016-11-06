662742ab169247e26f01608434c176f8ef193e19
James Richford <jamesrichford@googlemail.com>
2016-11-06 22:04:24 +0000

    remove FocusTests decorator

--- test/unit-tests/matchers/function-spy-call-count-matcher.spec.ts
+++ test/unit-tests/matchers/function-spy-call-count-matcher.spec.ts
@@ -1,31 +1,10 @@
-import { Expect, Test, FocusTests, FunctionSpy } from "../../../core/alsatian-core";
+import { Expect, Test, FunctionSpy } from "../../../core/alsatian-core";
 import { FunctionSpyCallCountMatcher } from "../../../core/matchers/function-spy-call-count-matcher";
 
-@FocusTests
 export class FunctionSpyCallCountMatcherTests {
 
    @Test()
    public timesNotToBeDefined() {
       Expect(new FunctionSpyCallCountMatcher(new FunctionSpy(), 0).times).not.toBeDefined();
    }
-
-   //Less than matches
-   //Not Less than matches
-
-   //Less than does not match
-   //Not Less than does not match
-
-   //More than matches
-   //Not more than matches
-
-   //More than does not match
-   //Not more than does not match
-
-   //exactly matches
-   //not exactly matches
-
-   //exactly does not match
-   //not exactly does not match
-
-   //invalid enum value
 }
