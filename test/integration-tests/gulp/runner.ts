2807682fbe6512b4a3f10c5dc86681550630a129
James Richford <jamesrichford@googlemail.com>
2016-11-17 09:04:41 +0000

    remove try block

--- test/integration-tests/gulp/runner.ts
+++ test/integration-tests/gulp/runner.ts
@@ -1,12 +1,13 @@
 import * as child from "child_process";
 import * as path from "path";
 import { Promise } from "../../../promise/promise";
-import { Expect, AsyncTest } from "../../../core/alsatian-core";
+import { Expect, AsyncTest, Timeout } from "../../../core/alsatian-core";
 import * as FileSystem from "fs";
 
 export class GulpIntegrationTests {
 
    @AsyncTest()
+   @Timeout(2000)
    public toBeExpectations() {
 
       const result = child.exec("gulp test-expectations --gulpfile \"./test/integration-tests/gulp/gulpfile.js\" --cwd ./");
@@ -19,14 +20,9 @@ export class GulpIntegrationTests {
       let expectedOutput = FileSystem.readFileSync("./test/integration-tests/expected-output/expectations/to-be.txt").toString();
 
       return new Promise((resolve, reject) => {
-         result.on("close", (code: number) => {
-             try {
-                Expect(consoleOutput).toBe(expectedOutput.replace(/\r/g, ""));
-                resolve();
-             }
-             catch(error) {
-                 reject(error);
-             }
+         result.on("close", (code: number) => {         
+            Expect(consoleOutput).toBe(expectedOutput.replace(/\r/g, ""));
+            resolve();
          });
       });
    }
