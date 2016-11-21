21df86b3cf39d96b7337249e2480d13938ca4da2
jamesrichford <jamesrichford@googlemail.com>
2016-11-21 12:47:26 +0000

    add TestFixture decorator

---
+++ core/decorators/test-fixture-decorator.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+import { TESTS } from "./_metadata-keys";
+
+export function TestFixture(description?: string) {
+  return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
+/*
+    // check if this has been registered as a test already
+    let tests: Array<any> = Reflect.getMetadata(TESTS, target);
+
+    // if there are no tests registered yet then register it
+    if (!tests) {
+      tests = [ {
+         key: propertyKey
+      } ];
+    }
+    // otherwise add it to the register
+    else if (tests.filter(test => test.key === propertyKey).length === 0) {
+      tests.push( {
+         key: propertyKey
+      });
+    }
+
+    // set the description
+    tests.filter(test => test.key === propertyKey)[0].description = description;
+
+    // update the register
+    Reflect.defineMetadata(TESTS, tests, target);
+};*/
+}
