7974b73160091f5eb3879ba63cbde3059e100f5a
jamesrichford <jamesrichford@googlemail.com>
2016-11-21 13:52:08 +0000

    TestFixture decorator functionality

--- core/decorators/test-fixture-decorator.ts
+++ core/decorators/test-fixture-decorator.ts
@@ -5,13 +5,13 @@ import { TestFixture as TestFixtureMetadata } from "../_core";
 export function TestFixture(description?: string) {
   
   return (constructor: Function) => {
-/*
+
     // create data about the test fixture
     // with the description either being the
     // given string or the class name
     const testFixtureMetadata = new TestFixtureMetadata(description || (<any>constructor).name);
 
     // attach meta data to the class
-    Reflect.defineMetadata(TEST_FIXTURE, testFixtureMetadata, constructor);*/
+    Reflect.defineMetadata(TEST_FIXTURE, testFixtureMetadata, constructor);
   };
 }
--- test/unit-tests/decorators/test-fixture-decorator.spec.ts
+++ test/unit-tests/decorators/test-fixture-decorator.spec.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { TestFixture as TestFixtureDecorator } from "../../../core/decorators/test-fixture-decorator";
-import { Expect, Test, TestFixture, TestCase, METADATA_KEYS, FocusTests } from "../../../core/alsatian-core";
+import { Expect, Test, TestFixture, TestCase, METADATA_KEYS, FocusTests, SpyOnProperty } from "../../../core/alsatian-core";
+import { TestFixture as TestFixtureMetadata } from "../../../core/_core";
 
 @FocusTests
 @TestFixture("Test Fixture Decorator Tests")
@@ -18,5 +19,49 @@ export class TestFixtureDecoratorTests {
 
         Expect(testFixtureMetadata).toBeDefined();
         Expect(testFixtureMetadata).not.toBeNull();
+    }
+
+    @Test()
+    public metaDataIsTestFixture() {
+        const testFixtureDecorator = TestFixtureDecorator();
+
+        const testFixtureConstructor = () => {};
+
+        testFixtureDecorator(testFixtureConstructor);
+
+        const testFixtureMetadata = Reflect.getMetadata(METADATA_KEYS.TEST_FIXTURE, testFixtureConstructor);
+
+        Expect(testFixtureMetadata instanceof TestFixtureMetadata).toBe(true);
+    }
+
+    @TestCase("something")
+    @TestCase("wow, this is super!")
+    @TestCase("Mega Hyper AWESOME test...")
+    public testFixtureMetadataDescriptionSet(testFixtureDescription: string) {
+        const testFixtureDecorator = TestFixtureDecorator(testFixtureDescription);
+
+        const testFixtureConstructor = () => {};
+
+        testFixtureDecorator(testFixtureConstructor);
+
+        const testFixtureMetadata = Reflect.getMetadata(METADATA_KEYS.TEST_FIXTURE, testFixtureConstructor);
+
+        Expect(testFixtureMetadata.description).toBe(testFixtureDescription);
+    }
+
+    @TestCase("Class")
+    @TestCase("TestFixture")
+    @TestCase("MegaHyperAwesomeTests")
+    public testFixtureMetadataDescriptionDefaultsToConstructorName(testFixtureClassName: string) {
+        const testFixtureDecorator = TestFixtureDecorator();
+
+        const testFixtureConstructor = () => {};
+        SpyOnProperty(testFixtureConstructor, "name").andReturnValue(testFixtureClassName);
+
+        testFixtureDecorator(testFixtureConstructor);
+
+        const testFixtureMetadata = Reflect.getMetadata(METADATA_KEYS.TEST_FIXTURE, testFixtureConstructor);
+
+        Expect(testFixtureMetadata.description).toBe(testFixtureClassName);
     }
 }
