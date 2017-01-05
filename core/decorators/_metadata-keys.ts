78e77dc2104317702e85f29b5af30d2ba8cc1579
jamesrichford <jamesrichford@googlemail.com>
2017-01-05 08:53:30 +0000

    type keys to string

--- core/decorators/_metadata-keys.ts
+++ core/decorators/_metadata-keys.ts
@@ -1,12 +1,12 @@
-const TEST_FIXTURE = "alsatian:test-fixture";
-const TESTS = "alsatian:tests";
-const FOCUS = "alsatian:focus";
-const IGNORE = "alsatian:ignore";
-const IGNORE_REASON = "alsatian:ignore-reason";
-const SETUP = "alsatian:setup";
-const TEARDOWN = "alsatian:teardown";
-const TEST_CASES = "alsatian:testcases";
-const TIMEOUT = "alsatian:timeout";
+const TEST_FIXTURE: string = "alsatian:test-fixture";
+const TESTS: string = "alsatian:tests";
+const FOCUS: string = "alsatian:focus";
+const IGNORE: string = "alsatian:ignore";
+const IGNORE_REASON: string = "alsatian:ignore-reason";
+const SETUP: string = "alsatian:setup";
+const TEARDOWN: string = "alsatian:teardown";
+const TEST_CASES: string = "alsatian:testcases";
+const TIMEOUT: string = "alsatian:timeout";
 
 export {
     TEST_FIXTURE,
