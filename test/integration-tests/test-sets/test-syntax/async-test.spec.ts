b4ad987164382c098730099c8838db010693fdfc
James Richford <jamesrichford@googlemail.com>
2017-01-08 15:06:12 +0000

    tidy integration test

--- test/integration-tests/test-sets/test-syntax/async-test.spec.ts
+++ test/integration-tests/test-sets/test-syntax/async-test.spec.ts
@@ -3,20 +3,20 @@ import { TestFixture, Expect, AsyncTest } from "../../../../core/alsatian-core";
 @TestFixture("asynchronous tests")
 export class AsyncTests {
 
-  private async _asyncFunction(): Promise<number> {
+   private async _asyncFunction(): Promise<number> {
        return new Promise<number>((resolve, reject) => {
            resolve(4);
        });
    }
 
-   @AsyncTest()
-   private async passingAsyncTest() {
+   @AsyncTest("simple passing asynchronous test")
+   public async passingAsyncTest() {
        const result = await this._asyncFunction();
        Expect(result).toBe(4);
    }
 
-   @AsyncTest()
-   private async failingAsyncTest() {
+   @AsyncTest("simple failing asynchronous test")
+   public async failingAsyncTest() {
        const result = await this._asyncFunction();
        Expect(result).toBe(5);
    }
