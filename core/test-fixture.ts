d9c4e8c1ec7c03497f14de380bab8ea4626513ed
James Richford <jamesrichford@googlemail.com>
2017-01-10 18:35:32 +0000

    update code style in line with linting

--- core/test-fixture.ts
+++ core/test-fixture.ts
@@ -1,15 +1,12 @@
-import { ITestFixture, ITest } from "./_interfaces";
+import { ITest, ITestFixture } from "./_interfaces";
 
 export class TestFixture implements ITestFixture {
 
-    fixture: { [id: string]: (...args: Array<any>) => any };
-
-    ignored: boolean;
-    ignoreReason: string;
-
-    focussed: boolean;
-
-    description: string;
+    public fixture: { [id: string]: (...args: Array<any>) => any };
+    public ignored: boolean;
+    public ignoreReason: string;
+    public focussed: boolean;
+    public description: string;
 
     private _tests: Array<ITest> = [];
     public get tests() {
