16a2b4f37a88c7390f3f6eff5112988653eee5f8
James Richford <jamesrichford@googlemail.com>
2017-01-10 18:41:07 +0000

    rework in line with new linting rules

--- core/spying/index.ts
+++ core/spying/index.ts
@@ -1,19 +1,19 @@
 import { Any } from "./any-argument";
+import { FunctionSpy } from "./function-spy";
 import { PropertySpy } from "./property-spy";
+import { RestorableFunctionSpy } from "./restorable-function-spy";
 import { SpyCall } from "./spy-call";
 import { SpyOn } from "./spy-on";
 import { SpyOnProperty } from "./spy-on-property";
-import { FunctionSpy } from "./function-spy";
-import { RestorableFunctionSpy } from "./restorable-function-spy";
 import { TypeMatcher } from "./type-matcher";
 
 export {
    Any,
+   FunctionSpy,
+   RestorableFunctionSpy,
    PropertySpy,
    SpyCall,
    SpyOn,
    SpyOnProperty,
-   FunctionSpy,
-   RestorableFunctionSpy,
    TypeMatcher
 };
