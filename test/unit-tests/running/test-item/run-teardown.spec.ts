7fc858ad0e6e6c050695afeeabc3d7aca797f19b
jamesrichford <jamesrichford@googlemail.com>
2017-01-13 17:56:35 +0000

    setup and teardown run tests

---
+++ test/unit-tests/running/test-item/run-teardown.spec.ts
@@ -0,0 +1,90 @@
+import { AsyncTest, TestCase, Timeout, Expect, SpyOn, METADATA_KEYS, FunctionSpy } from "../../../../core/alsatian-core";
+import { TestItem } from "../../../../core/running/test-item";
+import { TestSetBuilder } from "../../../builders/test-set-builder";
+import { TestBuilder } from "../../../builders/test-builder";
+import { TestFixtureBuilder } from "../../../builders/test-fixture-builder";
+import { MatchError } from "../../../../core/errors";
+
+export class TestItemRunTeardownTests {
+
+   @TestCase(1)
+   @TestCase(2)
+   @TestCase(8)
+   @AsyncTest()
+   public async successfulSyncTeardown(teardownFunctionCount: number) {
+      const test = new TestBuilder().withTestCaseCount(1).build();
+
+      const testFunction = () => {};
+
+      const testFixture = new TestFixtureBuilder()
+                                 .withFixture({
+                                    testFunction: testFunction
+                                 })
+                                 .addTest(test)
+                                 .build();   
+                                
+      const teardownFunctions = [];
+      const teardownFunctionInfo = [];
+      
+      for (let i = 0; i < teardownFunctionCount; i++) {
+          const teardownFunction = new FunctionSpy();
+          teardownFunctions.push(teardownFunction);
+          const teardownFunctionKey = "teardownFunction" + i + 1;
+          testFixture.fixture[teardownFunctionKey] = (teardownFunction as any);
+          teardownFunctionInfo.push({ propertyKey: teardownFunctionKey });
+      }
+
+      Reflect.defineMetadata(METADATA_KEYS.TEARDOWN, teardownFunctionInfo, testFixture.fixture);
+
+      const testItem = new TestItem(testFixture, test, test.testCases[0]);
+      
+      await testItem.run(500);
+
+      teardownFunctions.forEach(teardownFunction => {
+        Expect(teardownFunction).toHaveBeenCalled();
+      });
+   }
+
+   @TestCase(1)
+   @TestCase(2)
+   @TestCase(10)
+   @AsyncTest()
+   public async successfulAsyncTeardown(teardownFunctionCount: number) {
+      const test = new TestBuilder().withTestCaseCount(1).build();
+
+      const testFunction = () => {};
+
+      const testFixture = new TestFixtureBuilder()
+                                 .withFixture({
+                                    testFunction: testFunction
+                                 })
+                                 .addTest(test)
+                                 .build();   
+                                
+      const teardownFunctions = [];
+      const teardownFunctionInfo = [];
+      const teardownFunctionsCalled: Array<boolean> = [];
+      
+      for (let i = 0; i < teardownFunctionCount; i++) {
+          const teardownFunction = () => new Promise<void>((resolve, reject) => {
+                                          setTimeout(() => { teardownFunctionsCalled[i] = true; resolve(); }, 10);
+                                      });
+
+          teardownFunctions.push(teardownFunction);
+          const teardownFunctionKey = "teardownFunction" + i + 1;
+          testFixture.fixture[teardownFunctionKey] = teardownFunction;
+          teardownFunctionInfo.push({ propertyKey: teardownFunctionKey, isAsync: true });
+          teardownFunctionsCalled.push(false);
+      }
+
+      Reflect.defineMetadata(METADATA_KEYS.TEARDOWN, teardownFunctionInfo, testFixture.fixture);
+
+      const testItem = new TestItem(testFixture, test, test.testCases[0]);
+      
+      await testItem.run(500);
+
+      teardownFunctionsCalled.forEach(teardownFunctionCalled => {
+        Expect(teardownFunctionCalled).toBe(true);
+      });
+   }
+}
