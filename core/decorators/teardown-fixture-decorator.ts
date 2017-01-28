f937bc58de681d704dfed3387ae0c079987125e9
James Richford <jamesrichford@googlemail.com>
2017-01-28 12:20:01 +0000

    add @TeardownFixture decorator

---
+++ core/decorators/teardown-fixture-decorator.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { ISetupTeardownMetadata } from "./_interfaces";
+import { TEARDOWN_FIXTURE } from "./_metadata-keys";
+
+export function TeardownFixture(target: any, decoratedPropertyKey: string, descriptor: TypedPropertyDescriptor<() => any>) {
+
+    let teardownFixtureFunctions: Array<ISetupTeardownMetadata> = Reflect.getMetadata(TEARDOWN_FIXTURE, target);
+
+    if (!teardownFixtureFunctions) {
+      teardownFixtureFunctions = [];
+    }
+
+    teardownFixtureFunctions.push({
+        isAsync: false,
+        propertyKey: decoratedPropertyKey
+    });
+
+    // mark as teardown test method
+    Reflect.defineMetadata(TEARDOWN_FIXTURE, teardownFixtureFunctions, target);
+};
