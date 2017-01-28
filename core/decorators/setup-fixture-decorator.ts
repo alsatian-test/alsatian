e09d2a28654aac285e7d878e7ab31903c6191ed6
James Richford <jamesrichford@googlemail.com>
2017-01-28 12:16:42 +0000

    add @SetupFixture decorator

---
+++ core/decorators/setup-fixture-decorator.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { ISetupTeardownMetadata } from "./_interfaces";
+import { SETUP_FIXTURE } from "./_metadata-keys";
+
+export function SetupFixture(target: any, decoratedPropertyKey: string, descriptor: TypedPropertyDescriptor<() => any>) {
+
+    let setupFixtureFunctions: Array<ISetupTeardownMetadata> = Reflect.getMetadata(SETUP_FIXTURE, target);
+
+    if (!setupFixtureFunctions) {
+      setupFixtureFunctions = [];
+    }
+
+    setupFixtureFunctions.push({
+        isAsync: false,
+        propertyKey: decoratedPropertyKey
+    });
+
+    // mark as setup test method
+    Reflect.defineMetadata(SETUP_FIXTURE, setupFixtureFunctions, target);
+};
