cd619f4b2b60cb7da93155384ca758ae0d1dad86
James Monger <jameskmonger@hotmail.co.uk>
2017-03-29 23:07:34 +0100

    Fix errors

--- core/_interfaces/index.ts
+++ core/_interfaces/index.ts
@@ -1,9 +1,11 @@
 import { ITestCase } from "./test-case.i";
 import { ITestFixture } from "./test-fixture.i";
 import { ITest } from "./test.i";
+import { INameable } from "./nameable.i";
 
 export {
     ITestCase,
     ITestFixture,
-    ITest
+    ITest,
+    INameable
 };
--- core/errors/function-call-count-match-error.ts
+++ core/errors/function-call-count-match-error.ts
@@ -1,6 +1,7 @@
 import { SpyCallCountType } from "../matchers";
 import { Any, FunctionSpy, TypeMatcher } from "../spying";
 import { MatchError } from "./match-error";
+import { INameable } from "../_interfaces";
 
 export class FunctionCallCountMatchError extends MatchError {
 
@@ -41,7 +42,7 @@ export class FunctionCallCountMatchError extends MatchError {
                   return "Anything";
                }
                else if (arg instanceof TypeMatcher) {
-                  return "Any " + (<any> arg.type).name;
+                  return "Any " + (arg.type as INameable).name;
                }
                else {
                   return JSON.stringify(arg);
