9d01b81fe303f6dfb1c893e43ef5e2c4769c79d7
James Monger <jameskmonger@hotmail.co.uk>
2017-03-29 23:21:47 +0100

    Fix errors in Expect

--- core/expect.ts
+++ core/expect.ts
@@ -254,7 +254,7 @@ export class Matcher {
       }
 
       if (threwRightError !== this.shouldMatch) {
-         throw new ErrorMatchError(actualError, this.shouldMatch, (<any> errorType), errorMessage);
+         throw new ErrorMatchError(actualError, this.shouldMatch, (errorType as any), errorMessage);
       }
    }
 
@@ -340,8 +340,8 @@ export class Matcher {
       }
 
       // get all the property keys for each object
-      let objectAKeys = Object.keys(objectA);
-      let objectBKeys = Object.keys(objectB);
+      const objectAKeys = Object.keys(objectA);
+      const objectBKeys = Object.keys(objectB);
 
       // if they don't have the same amount of properties then clearly not
       if (objectAKeys.length !== objectBKeys.length) {
