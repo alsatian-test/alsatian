e3759f2012386cb963e69f505c7aa9b549a83b36
James Richford <jamesrichford@googlemail.com>
2017-04-03 18:11:59 +0100

    get some debug on

--- test/integration-tests/typescript/compile.spec.ts
+++ test/integration-tests/typescript/compile.spec.ts
@@ -13,10 +13,16 @@ export class TypeScriptCompilerTests {
    @Timeout(5000)
    public targetBuildCompiles(target: string) {
 
-      const result = child.exec("tsc -p ./tsconfig.json --target " + target);
+      const result = child.exec("tsc -p ./test/integration-tests/typescript/tsconfig.json --target " + target);
+      
+      let consoleOutput = "";
+
+      result.stdout.on("data", (data) => consoleOutput += data);
+      result.stderr.on("data", (data) => consoleOutput += data);
 
       return new Promise<void>((resolve, reject) => {
          result.on("close", (code: number) => {
+          console.log(consoleOutput);
             Expect(code).toBe(0);
             resolve();
          });
