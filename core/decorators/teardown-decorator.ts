1aa4267ac44c2e40eea96c33be176b2564371a57
James Richford <jamesrichford@googlemail.com>
2017-04-05 17:22:04 +0100

    break up long line

--- core/decorators/teardown-decorator.ts
+++ core/decorators/teardown-decorator.ts
@@ -2,7 +2,9 @@ import "reflect-metadata";
 import { ISetupTeardownMetadata } from "./_interfaces";
 import { TEARDOWN } from "./_metadata-keys";
 
-export function Teardown(target: Object, decoratedPropertyKey: string, descriptor?: TypedPropertyDescriptor<() => any>) {
+export function Teardown(target: Object,
+                         decoratedPropertyKey: string,
+                         descriptor?: TypedPropertyDescriptor<() => any>) {
 
     let teardownFunctions: Array<ISetupTeardownMetadata> = Reflect.getMetadata(TEARDOWN, target);
 
