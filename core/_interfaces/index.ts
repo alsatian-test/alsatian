a321b2ef297d32e8eb8897427199bde28837ffeb
James Monger <jameskmonger@hotmail.co.uk>
2017-05-10 23:13:17 +0100

    Update index.ts

--- core/_interfaces/index.ts
+++ core/_interfaces/index.ts
@@ -2,7 +2,7 @@ import { INameable } from "./nameable.i";
 import { ITestCase } from "./test-case.i";
 import { ITestFixture } from "./test-fixture.i";
 import { ITest } from "./test.i";
-import { Constructor } from "./constructor.i";
+import { Constructor } from "./constructor";
 import { ITester } from "./tester.i";
 import { ISpyCall } from "./spy-call.i";
 
