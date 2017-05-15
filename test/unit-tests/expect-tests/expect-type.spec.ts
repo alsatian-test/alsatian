3800cc89191d2c595272787d0917f1f6ccb41628
James Richford <jamesrichford@googlemail.com>
2017-05-15 18:56:51 +0100

    first set of runtime tests

---
+++ test/unit-tests/expect-tests/expect-type.spec.ts
@@ -0,0 +1,25 @@
+import { Expect, Test, TestCase, TestFixture, FocusTests } from "../../../core/alsatian-core";
+
+@FocusTests
+@TestFixture("correct matchers returned for tests")
+export class ExpectTypeTests {
+
+    @TestCase({})
+    @TestCase(new Error("something strange"))
+    @Test("unknown type gets general functionality")
+    public anyGetsDefault(actual: any) {
+        Expect(Expect(actual).toBe).toBeDefined();
+        Expect(Expect(actual).toBeDefined).toBeDefined();
+        Expect(Expect(actual).toBeNull).toBeDefined();
+        Expect(Expect(actual).toBeTruthy).toBeDefined();
+        Expect(Expect(actual).toEqual).toBeDefined();
+
+    }
+
+    @TestCase({})
+    @TestCase(new Error("something strange"))
+    @Test("unknown type gets no special type functionality")
+    public anyDoesNotGetSpecialFunctions(actual: any) {
+        Expect((Expect(actual) as any)["toMatch"]).not.toBeDefined();
+    }
+}
