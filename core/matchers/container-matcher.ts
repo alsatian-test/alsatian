442e116096d09947dda3a9164070833143e28434
James Richford <jamesrichford@googlemail.com>
2017-05-17 22:15:53 +0100

    remove duplicate definition

--- core/matchers/container-matcher.ts
+++ core/matchers/container-matcher.ts
@@ -21,17 +21,4 @@ export class ContainerMatcher<ContainerType, ContentType> extends EmptyMatcher<C
          throw new ContentsMatchError(this.actualValue, expectedContent, this.shouldMatch);
       }
    }
-
-   /**
-    * Checks that an array is empty, a string is empty, or an object literal has no properties
-    */
-   public toBeEmpty() {
-      if (null === this.actualValue || undefined === this.actualValue) {
-         throw new TypeError("toBeEmpty requires value passed in to Expect not to be null or undefined");
-      }
-
-      if (((this.actualValue as any).length === 0) !== this.shouldMatch) {
-         throw new EmptyMatchError(this.actualValue, this.shouldMatch);
-      }
-   }
 }
