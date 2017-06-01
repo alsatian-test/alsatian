4e54dc9719680362bcb87358d50c1450fc6c43ff
James Richford <jamesrichford@googlemail.com>
2017-06-01 08:10:40 +0100

    create number padder

---
+++ scripts/pad-number.ts
@@ -0,0 +1,23 @@
+let zeroCache = "0000000000";
+
+export function padNumber(originalNumber: number, minimumIntegerLength: number) {
+    if (zeroCache.length < minimumIntegerLength) {
+        zeroCache = new Array(minimumIntegerLength  + 1).join("0");
+    }
+
+    const absoluteNumber = Math.abs(originalNumber);
+
+    const integerLength = Math.trunc(absoluteNumber).toString().length;
+
+    if (integerLength >= minimumIntegerLength) {
+        return originalNumber.toString();
+    }
+
+    const numberString = zeroCache.slice(0, minimumIntegerLength - integerLength) + absoluteNumber.toString();
+
+    if (originalNumber < 0) {
+        return "-" + numberString;
+    }
+
+    return numberString;
+}
