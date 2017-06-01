f8a9f66e2f712eccb3c5ab30e8ff178eceb41478
James Richford <jamesrichford@googlemail.com>
2017-06-02 00:56:03 +0100

    resolve linting

--- scripts/build-nightly.ts
+++ scripts/build-nightly.ts
@@ -1,8 +1,8 @@
 import { spawn } from "child_process";
 import { GitProcess } from "dugite";
 import { writeFile } from "fs";
-import { padNumber } from "./pad-number";
 import { getPackageJson } from "./get-package-json";
+import { padNumber } from "./pad-number";
 
 GitProcess.exec([ "log", "-1", "--format=%cd"], "./").then(x => {
     const lastCommitDate = new Date(x.stdout);
@@ -17,15 +17,16 @@ GitProcess.exec([ "log", "-1", "--format=%cd"], "./").then(x => {
     else {
 
         const packageJson = getPackageJson();
-        packageJson.version = packageJson.version.split("-")[0] + "-" + now.getFullYear() + padNumber(now.getMonth() + 1, 2) + padNumber(now.getDate(), 2);
+        packageJson.version = packageJson.version.split("-")[0] + "-" +
+                              now.getFullYear() + padNumber(now.getMonth() + 1, 2) + padNumber(now.getDate(), 2);
 
-        writeFile("./package.json", JSON.stringify(packageJson, null, 3), (error) => {   
+        writeFile("./package.json", JSON.stringify(packageJson, null, 3), (error) => {
             if (error) {
                 process.stderr.write("publish preparation failed: " + error.message);
                 process.exit(1);
             }
             else {
-                process.stdout.write(packageJson.version + " ready to publish\n");         
+                process.stdout.write(packageJson.version + " ready to publish\n");
                 process.exit(0);
             }
         });
