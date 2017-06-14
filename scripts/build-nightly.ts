355fb0a13f99883f837df55d0f8c23d9601c26fc
James Richford <jrichford@neworbit.co.uk>
2017-06-14 12:22:47 +0100

    move building .npmrc to build script

--- scripts/build-nightly.ts
+++ scripts/build-nightly.ts
@@ -4,8 +4,8 @@ import { writeFile } from "fs";
 import { getPackageJson } from "./get-package-json";
 import { padNumber } from "./pad-number";
 
-GitProcess.exec([ "log", "-1", "--format=%cd"], "./").then(x => {
-    const lastCommitDate = new Date(x.stdout);
+GitProcess.exec([ "log", "-1", "--format=%cd"], "./").then(async result => {
+    const lastCommitDate = new Date(result.stdout);
     const now = new Date();
 
     const ONE_DAY_IN_MILLISECONDS = 86400000;
@@ -23,26 +23,48 @@ GitProcess.exec([ "log", "-1", "--format=%cd"], "./").then(x => {
         const packageJson = getPackageJson();
         packageJson.version = packageJson.version.split("-")[0] + "-" +
                               now.getFullYear() + padNumber(now.getMonth() + 1, 2) + padNumber(now.getDate(), 2);
-
-        writeFile("./package.json", JSON.stringify(packageJson, null, 3), (error) => {
-            if (error) {
-                process.stderr.write("publish preparation failed: " + error.message);
-                process.exit(1);
+        
+        try {
+            await writeFileAsync("./package.json", JSON.stringify(packageJson, null, 3));
+            if (process.env.NPM_AUTH_TOKEN) {
+                await writeFileAsync("./.npmrc", "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}\n");
             }
-            else {
-
-                process.stdout.write(packageJson.version + " ready to publish\n");
-
-                const publishProcess = spawn("npm", [ "publish", "--tag", "next" ]);
-
-                publishProcess.on("close", (code, signal) => {
-                    process.stdout.write(packageJson.version + " publishedh\n");
-                    process.exit(0);
-                });
-
-                publishProcess.stdout.pipe(process.stdout);
-                publishProcess.stderr.pipe(process.stderr);
-            }
-        });
+            process.stdout.write(packageJson.version + " ready to publish\n");
+            await npmPublish();
+            process.stdout.write(packageJson.version + " publishedh\n");
+            process.exit(0);
+        }
+        catch(error) {
+            process.stderr.write("publish failed: " + error.message);
+            process.exit(1);            
+        }
     }
 });
+
+async function writeFileAsync(fileLocation: string, fileContents: string) {
+    return new Promise((resolve, reject) => {
+        writeFile(fileLocation, fileContents, (error: Error) => {
+            
+            if (error) {
+                return reject(error);
+            }
+            
+            resolve();
+        });
+    });
+}
+
+async function npmPublish() {
+
+    return new Promise((resolve, reject) => {
+        
+        const publishProcess = spawn("npm", [ "publish", "--tag", "next" ]);
+
+        publishProcess.on("close", (code, signal) => {
+            resolve();
+        });
+
+        publishProcess.stdout.pipe(process.stdout);
+        publishProcess.stderr.pipe(process.stderr);
+    });
+}
