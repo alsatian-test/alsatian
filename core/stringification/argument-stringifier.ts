48660110fb4d6c3bb040077c37153e0832d13166
James Richford <jamesrichford@googlemail.com>
2017-07-14 23:25:42 +0100

    remove old argument stringifier

--- core/stringification/argument-stringifier.ts
+++
@@ -1,9 +0,0 @@
-import { Any, TypeMatcher } from "../spying";
-import { stringify } from "./stringify";
-
-export class ArgumentStringifier {
-
-    public stringify(argument: any) {
-        return stringify(argument);
-    }
-}
