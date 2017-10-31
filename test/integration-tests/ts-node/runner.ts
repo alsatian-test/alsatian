508367137f9a71bb499cbff44211034a24806928
James Richford <jrichford@neworbit.co.uk>
2017-10-31 18:35:59 +0000

    add ts-node integration tests

---
+++ test/integration-tests/ts-node/runner.ts
@@ -0,0 +1,26 @@
+import { TapBark } from "tap-bark";
+import { TestSet, TestRunner } from "alsatian";
+
+async function runTypeScriptTests () {
+    try {
+        const testSet = TestSet.create();
+        testSet.addTestsFromFiles('./test/integration-tests/test-sets/**/*.spec.ts');
+
+        const testRunner = new TestRunner();
+
+        testRunner.outputStream
+            .pipe(TapBark.create().getPipeable())
+            .pipe(process.stdout);
+
+        await testRunner.run(testSet);
+
+        // if we got this far then everything ran fine! :)
+        process.exit(0);
+    }
+    catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+runTypeScriptTests();
