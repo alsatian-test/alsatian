79a9a45ea93e5da898eb70401000e0d0d816a174
James Richford <jrichford@neworbit.co.uk>
2018-01-23 00:02:52 +0000

    further function spy matcher refactors

--- core/matchers/function-spy-matcher.ts
+++ core/matchers/function-spy-matcher.ts
@@ -24,15 +24,9 @@ export class FunctionSpyMatcher {
 
     this._validateCallCount(expectedCallCount, "expectedCallCount");
 
-      if (this._expectedArguments
-       && this._getArgumentsMatching(this._expectedArguments).length !== expectedCallCount) {
-         throw new FunctionCallCountMatchError(
-             this._spy,
-             true,
-             expectedCallCount,
-             SpyCallCountType.Exactly,
-             this._expectedArguments
-         );
+      if (this._matchingArguments() !== expectedCallCount) {
+         
+        this._throwCallCountError(expectedCallCount, SpyCallCountType.Exactly, true);
       }
       else if (this._spy.calls.length !== expectedCallCount) {
          throw new FunctionCallCountMatchError(this._spy, true, expectedCallCount, SpyCallCountType.Exactly);
@@ -45,15 +39,9 @@ export class FunctionSpyMatcher {
 
     this._validateCallCount(unexpectedCallCount, "unexpectedCallCount");
 
-      if (this._expectedArguments
-       && this._getArgumentsMatching(this._expectedArguments).length === unexpectedCallCount) {
-         throw new FunctionCallCountMatchError(
-             this._spy,
-             false,
-             unexpectedCallCount,
-             SpyCallCountType.Exactly,
-             this._expectedArguments
-         );
+      if (this._matchingArguments() === unexpectedCallCount) {
+         
+        this._throwCallCountError(unexpectedCallCount, SpyCallCountType.Exactly, false);
       }
       else if (this._spy.calls.length === unexpectedCallCount) {
          throw new FunctionCallCountMatchError(this._spy, false, unexpectedCallCount, SpyCallCountType.Exactly);
@@ -66,15 +54,8 @@ export class FunctionSpyMatcher {
 
     this._validateCallCount(minimumCallCount, "minimumCallCount");
 
-      if (this._expectedArguments
-       && this._getArgumentsMatching(this._expectedArguments).length <= minimumCallCount) {
-         throw new FunctionCallCountMatchError(
-             this._spy,
-             true,
-             minimumCallCount,
-             SpyCallCountType.GreaterThan,
-             this._expectedArguments
-         );
+      if (this._matchingArguments() <= minimumCallCount) {          
+         this._throwCallCountError(minimumCallCount, SpyCallCountType.GreaterThan, true);
       }
       else if (this._spy.calls.length <= minimumCallCount) {
          throw new FunctionCallCountMatchError(this._spy, true, minimumCallCount, SpyCallCountType.GreaterThan);
@@ -87,15 +68,8 @@ export class FunctionSpyMatcher {
 
       this._validateCallCount(maximumCallCount, "maximumCallCount");
 
-      if (this._expectedArguments
-      && this._getArgumentsMatching(this._expectedArguments).length >= maximumCallCount) {
-         throw new FunctionCallCountMatchError(
-             this._spy,
-             true,
-             maximumCallCount,
-             SpyCallCountType.LessThan,
-             this._expectedArguments
-         );
+      if (this._matchingArguments() >= maximumCallCount) {
+         this._throwCallCountError(maximumCallCount, SpyCallCountType.LessThan, true);
       }
       else if (this._spy.calls.length >= maximumCallCount) {
          throw new FunctionCallCountMatchError(this._spy, true, maximumCallCount, SpyCallCountType.LessThan);
@@ -112,5 +86,23 @@ export class FunctionSpyMatcher {
     if (callCount < 1) {
         throw new TypeError(`${callCountName} must be greater than 0.`);
      }
+   }
+
+   private _matchingArguments() {
+       if (this._expectedArguments === null) {
+           return;
+       }
+
+       return this._spy.callsWithArguments.apply(this._spy, this._expectedArguments).length;
+   }
+
+   private _throwCallCountError(callCount: number, callCountType: SpyCallCountType, shouldMatch: boolean) {
+    throw new FunctionCallCountMatchError(
+        this._spy,
+        shouldMatch,
+        callCount,
+        callCountType,
+        this._expectedArguments
+    );
    }
 }
