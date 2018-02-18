ceba5820ee998dacd8be54ac295a47959b205b21
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2018-02-18 18:37:21 -0300

    Document truthy-match-error.ts

--- core/errors/truthy-match-error.ts
+++ core/errors/truthy-match-error.ts
@@ -1,7 +1,21 @@
 import { MatchError } from "./match-error";
 import { stringify } from "../stringification";
 
+/**
+ * Represents an error thrown in a failed
+ * Matcher.toBeTruthy() or Matcher.toBeFalsy() statement.
+ *
+ * @export
+ * @class TruthyMatchError
+ * @extends {MatchError}
+ */
 export class TruthyMatchError extends MatchError {
+  /**
+   * Creates an instance of TruthyMatchError.
+   * @param {*} actualValue The actual received value
+   * @param {boolean} shouldMatch The expected boolean value
+   * @memberof TruthyMatchError
+   */
   public constructor(actualValue: any, shouldMatch: boolean) {
     super(
       `Expected ${stringify(actualValue)} ${
@@ -10,6 +24,6 @@ export class TruthyMatchError extends MatchError {
     );
 
     this._actual = actualValue;
-    this._expected = `${shouldMatch ? "truthy" : "falsy"}`;
+    this._expected = shouldMatch ? "truthy" : "falsy";
   }
 }
