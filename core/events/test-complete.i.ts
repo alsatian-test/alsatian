ee9ffb0a45ddc16bd35677c8847900b1b00f17e7
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2018-02-18 18:11:49 -0300

    Document test-complete.i.ts

--- core/events/test-complete.i.ts
+++ core/events/test-complete.i.ts
@@ -1,6 +1,11 @@
 import { ITest, ITestCase, ITestFixture } from "../_interfaces/index";
 import { TestOutcome } from "../results/test-outcome";
 
+/**
+ * Describes the complete event for a test
+ * @export
+ * @interface ITestCompleteEvent
+ */
 export interface ITestCompleteEvent {
   testId: number;
   testCase: ITestCase;
