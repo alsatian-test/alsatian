4f6647ff656bb124b71ed183d9bba346010ab73c
James Adarich <jadarich@neworbit.co.uk>
2018-10-08 23:58:07 +0100

    export focus and ignore

--- core/decorators/index.ts
+++ core/decorators/index.ts
@@ -3,8 +3,10 @@ import { AsyncSetupFixture } from "./async-setup-fixture-decorator";
 import { AsyncTeardown } from "./async-teardown-decorator";
 import { AsyncTeardownFixture } from "./async-teardown-fixture-decorator";
 import { AsyncTest } from "./async-test-decorator";
+import { Focus } from "./focus-decorator";
 import { FocusTest } from "./focus-test-decorator";
 import { FocusTests } from "./focus-tests-decorator";
+import { Ignore } from "./ignore-decorator";
 import { IgnoreTest } from "./ignore-test-decorator";
 import { IgnoreTests } from "./ignore-tests-decorator";
 import { Setup } from "./setup-decorator";
@@ -23,8 +25,10 @@ export {
   AsyncTeardown,
   AsyncTeardownFixture,
   AsyncTest,
+  Focus,
   FocusTest,
   FocusTests,
+  Ignore,
   IgnoreTest,
   IgnoreTests,
   Setup,
