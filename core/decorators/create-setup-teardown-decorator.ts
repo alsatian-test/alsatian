1e47cd5194f8297de2e97b6b1a5d2ec673c9a450
James Adarich <jadarich@neworbit.co.uk>
2018-10-18 23:22:10 +0100

    don't warn for setup and teardown decorators

--- core/decorators/create-setup-teardown-decorator.ts
+++ core/decorators/create-setup-teardown-decorator.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { ISetupTeardownMetadata } from "./_interfaces";
 import { deprecate } from "../maintenance/deprecate";
+import { SETUP_FIXTURE } from "./_metadata-keys";
 
 export function createSetupTeardownDecorator(
   metadataDescription: string,
@@ -20,11 +21,13 @@ export function createSetupTeardownDecorator(
       .replace("setup", "Setup")
       .replace("teardown", "Teardown");
 
-    deprecate(
-      isAsync ? `Async${functionName}` : functionName,
-      "4.0.0",
-      `Use the ${functionName.replace("Fixture", "")} decorator instead.`
-    );
+    if (metadataDescription.indexOf("-fixture") > -1 || isAsync) {
+      deprecate(
+        isAsync ? `Async${functionName}` : functionName,
+        "4.0.0",
+        `Use the ${functionName.replace("Fixture", "")} decorator instead.`
+      );
+    }
 
     functions.push({
       isAsync,
