78ddb70b5b8ca605b5cc0db17037cd8dd4c4958f
James Adarich <jadarich@neworbit.co.uk>
2018-10-23 22:40:10 +0100

    move coverage report to shell script

---
+++ .codeclimate-report-coverage.sh
@@ -0,0 +1,9 @@
+# create json reports from lcov
+./cc-test-reporter format-coverage -t lcov -o coverage/alsatian.json packages/alsatian/coverage/lcov.info
+./cc-test-reporter format-coverage -t lcov -o coverage/tap-bark.json packages/alsatian/coverage/lcov.info
+
+# sum coverage together
+./cc-test-reporter sum-coverage coverage/*.json -p 2
+
+# upload to codeclimate
+./cc-test-reporter upload-coverage
