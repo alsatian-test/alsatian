8711631d7dedcf90cf301238e8fc634b1f8716b1
Thibaut Colar <tcolar@colar.net>
2019-07-05 15:51:18 -0700

    Port the test from https://github.com/alsatian-test/alsatian/pull/529

--- packages/alsatian/test/integration-tests/cli/runner.ts
+++ packages/alsatian/test/integration-tests/cli/runner.ts
@@ -67,4 +67,22 @@ export class CliIntegrationTests {
 			});
 		});
 	}
+
+	@AsyncTest()
+	public async tapWithTimeoutDoesNotDelayShutdown() {
+		const result = child.exec(
+			`alsatian ` +
+				`./test/integration-tests/cli/single.spec.js` +
+				`--tap` +
+				`--timeout` +
+				`9999`
+		);
+		// This does not test aything other than it will timeout, because
+		// it's awaiting the timeouts set in test-item (for 9999 seconds)
+		return new Promise<void>((resolve, reject) => {
+			result.on("close", (code: number) => {
+				resolve();
+			});
+		});
+	}
 }
---
+++ packages/alsatian/test/integration-tests/single.spec.ts
@@ -0,0 +1,8 @@
+import { Expect, Test } from "alsatian";
+
+export class SingleTest {
+  @Test()
+  public twoPlusTwoMakeFour() {
+	Expect(2 + 2).toBe(4);
+  }
+}
