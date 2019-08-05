edb0242a5e429b1465e761cf42ad83a9ccb03aa7
James Monger <jameskmonger@hotmail.co.uk>
2019-08-05 10:03:14 +0100

    Add toEqual integration test

---
+++ packages/alsatian/test/integration-tests/test-sets/expectations/to-equal.spec.ts
@@ -0,0 +1,16 @@
+import { Expect, Test } from "alsatian";
+
+export class ToEqualTests {
+	@Test()
+	public onObjectWithNumericDifference() {
+        const expected = {
+            price: 160
+        };
+
+        const result = {
+            price: 159.99999999999997
+        };
+
+        Expect(result).toEqual(expected);
+	}
+}
