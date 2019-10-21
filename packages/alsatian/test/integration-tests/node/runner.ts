a718c2814bb7ce7718f21479987832cf9c748449
James Adarich <jadarich@neworbit.co.uk>
2019-10-21 22:58:30 +0100

    fix node unit-tests

--- packages/alsatian/test/integration-tests/node/runner.ts
+++ packages/alsatian/test/integration-tests/node/runner.ts
@@ -2,7 +2,7 @@ import * as Alsatian from "../../../core/alsatian-core";
 
 // create test set
 const testSet = Alsatian.TestSet.create();
-testSet.addTestsFromFiles("./test/integration-tests/test-sets/**/*.spec.js");
+testSet.addTestsFromFiles("./dist/test/integration-tests/test-sets/**/*.spec.js");
 
 // run the test set
 const testRunner = new Alsatian.TestRunner();
