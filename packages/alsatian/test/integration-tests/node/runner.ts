d44ff4e5b75c26fc9aa4894829d6ef089c72c608
James Adarich <jadarich@neworbit.co.uk>
2019-10-22 23:49:36 +0100

    fix linting issues

--- packages/alsatian/test/integration-tests/node/runner.ts
+++ packages/alsatian/test/integration-tests/node/runner.ts
@@ -2,26 +2,26 @@ import { TapBark } from "tap-bark";
 import { TestSet, TestRunner } from "alsatian";
 
 async function runNodeTests() {
-    try {
-        // create test set
-        const testSet = TestSet.create();
-        testSet.addTestsFromFiles("./dist/test/integration-tests/test-sets/**/*.spec.js");
+	try {
+		// create test set
+		const testSet = TestSet.create();
+		testSet.addTestsFromFiles("./dist/test/integration-tests/test-sets/**/*.spec.js");
 
-        // run the test set
-        const testRunner = new TestRunner();
+		// run the test set
+		const testRunner = new TestRunner();
 
-        // pipe output to console
-        testRunner.outputStream
-            .pipe(TapBark.create().getPipeable())
-            .pipe(process.stdout);
+		// pipe output to console
+		testRunner.outputStream
+			.pipe(TapBark.create().getPipeable())
+			.pipe(process.stdout);
 
-        testRunner.run(testSet);
-        // if we got this far then everything ran fine! :)
-        process.exit(0);
-    } catch (error) {
-        process.stderr.write(error);
-        process.exit(1);
-    }
+		testRunner.run(testSet);
+		// if we got this far then everything ran fine! :)
+		process.exit(0);
+	} catch (error) {
+		process.stderr.write(error);
+		process.exit(1);
+	}
 }
 
 runNodeTests();
