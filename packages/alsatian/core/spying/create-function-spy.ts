4e74e688108cee23c21b3fd1765258ece52c6ff5
Henry Ing-Simmons <hing-simmons@neworbit.co.uk>
2019-12-06 13:57:56 +0000

    Update typings for ISpiedFunction

--- packages/alsatian/core/spying/create-function-spy.ts
+++ packages/alsatian/core/spying/create-function-spy.ts
@@ -1,13 +1,13 @@
 import { FunctionSpy } from "./function-spy";
 import { exposeSpyFunctions } from "./expose-spy-functions";
-import { ISpiedFunction } from "./spied-function.i";
+import { ISpiedFunction, TypedFunction } from "./spied-function.i";
 
-export function createFunctionSpy<FunctionType extends (...args: Array<any>) => any>(): FunctionType & FunctionSpy;
-export function createFunctionSpy<ArgumentType, ReturnType>(): ISpiedFunction<ArgumentType, ReturnType>;
-export function createFunctionSpy<ArgumentType, ReturnType>(): ISpiedFunction<
+export function createFunctionSpy<FunctionType extends TypedFunction>(): ISpiedFunction<FunctionType>;
+export function createFunctionSpy<
 	ArgumentType,
 	ReturnType
-> {
+>(): ISpiedFunction<(...args: Array<ArgumentType>) => ReturnType>;
+export function createFunctionSpy() {
 	const functionSpy = new FunctionSpy();
 
 	const spiedFunction = functionSpy.call.bind(functionSpy);
