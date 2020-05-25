b8c069f0c3abfa677f5253bc1e840a59afcbaacf
Mateusz Wolochow <mwolochow@neworbit.co.uk>
2020-05-25 21:18:01 +0200

    stop

--- packages/alsatian-property-testing/sample.ts
+++ packages/alsatian-property-testing/sample.ts
@@ -8,7 +8,7 @@ import { Numbers } from "./src/numbers";
 export class PropertyTests {
     @Test()
     public test() {
-        var generator = new Objects<TestInterface>({propTwo: "duuupa"}).withProp("propThree", 2).withProp("propOne", "test").build(20)();
+        var generator = new Objects<TestInterface>({propTwo: "duuupa"}).withProp("propThree", Strings.Below(10).generate(10)()).withProp("propOne", Numbers.Between(1, 6, 1)()).build(20)();
 
 
         var g1 = generator.next().value;
@@ -22,33 +22,24 @@ export class PropertyTests {
     }
 }
 
-class ObjectPropertyBuilder<T = any> {
-
-    with(callback: (obj: T) => void) {
-        return this;
-    }
-
-    build(count: number) {
-        return function*() {
-
-        }
-    }
-}
-
-// THESE SHOULD LIKELY BE BUILDERS ENDING WITH buildCount(x: number) to return the generator
 class Objects<T = { [prop: string]: any }> {
     private constructedObject: Partial<T>;
+    private valueMatrix: {[id in keyof T]: any[] };
 
-    // private propGenerators: { propName: keyof T, generaotr: Generator<number | string, void, unknown>[] };
-
-    // For things that we might want to be constant like ids or other props. I think that might make life easier
     constructor(object?: Partial<T>) {
        this.constructedObject = { ...object };
     }
 
-    // parametrem moze byc albo generator.... albo generator builder? jak to sprytnie owinac
-    withProp(propName: keyof T, value: any /*, generator: Generator<number | string, void, unknown>*/) {
-        this.constructedObject = { ...this.constructedObject, [propName]: value}
+    withProp(propName: keyof T, generator: Generator<number | string | object, void, unknown>) {
+        let iteration = generator.next();
+        let values = [];
+
+        while(iteration.done === false) {
+            values.push(iteration.value);
+            iteration = generator.next();
+        };
+
+        this.valueMatrix[propName] = values;
         return this;
     }
 
@@ -62,7 +53,13 @@ class Objects<T = { [prop: string]: any }> {
 
         return function* () {
             while(counter <= count) {
-                yield { ...self.constructedObject };
+                let obj = { ...self.constructedObject };
+
+                for (let key in self.valueMatrix) {
+                    obj = {...obj, [key]: self.valueMatrix[key][Math.floor(Math.random() * self.valueMatrix[key].length)]}
+                }
+
+                yield obj;
                 counter++;
             }
         }
@@ -74,58 +71,3 @@ interface TestInterface {
     propTwo: string;
     propThree: number;
 }
-
-
-// import { Expect, TestFixture, Test, TestCases } from "alsatian";
-// import { Integers } from "./src/integers";
-
-// @TestFixture("property tests")
-// export class PropertyTests {
-
-//     @TestCases(Integers.between(0, 4))
-//     @TestCases(Integers.random({ between: { lowerLimit: 1000, upperLimit: 1000000 }}).generate(16))
-//     @Test("sample integer test")
-//     public test(value: number) {
-//         Expect(value).toBeLessThan(4);
-//     }
-
-// /*
-//     //TODO: what if the two generators result in two different amounts of arguments?
-//     @TestCases(Integers.Between(0, 42), Integers.Between(1, 43))
-//     @Test("sample two integer test")
-//     public twoIntegerSample(small: number, big: number) {
-//         Expect(small).toBeLessThan(big);
-//     }
-//     @TestCases(Objects.with(x => x.name = randomString()).with(x => x).build(42))
-//     @TestCases(Objects<{ name: string }>.with(x => x.name = randomString()).with(x => x).build(42))
-//     @TestCases(Strings.ofLength(12).withCharSet(utf8).build(42))*/
-// }
-
-
-// class ObjectPropertyBuilder<T = any> {
-
-//     with(callback: (obj: T) => void) {
-//         return this;
-//     }
-
-//     build(count: number) {
-//         return function*() {
-
-//         }
-//     }
-// }
-
-// // THESE SHOULD LIKELY BE BUILDERS ENDING WITH buildCount(x: number) to return the generator
-// class Objects<T = { [prop: string]: any }> {
-//     /*static with(assign: (obj: T) => void) {
-//         return new ObjectPropertyBuilder<T>().with(assign);
-//     }*/
-// }
-
-// class Arrays {
-//     static Random<V>(count: number, options?: { length: () => number, fill: () => V }) {
-//         return function*() {
-
-//         }
-//     }
-// }
