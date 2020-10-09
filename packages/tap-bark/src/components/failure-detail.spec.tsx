872e789b38d662673ff0e62328adbb194217796e
James Adarich <james.adarich@gmail.com>
2020-10-09 11:21:09 +0100

    fix render when using empty object
    Co-authored-by: Rob James <rjames@neworbit.co.uk>
    Co-authored-by: Dawid Dragan <ddragan@neworbit.co.uk>
    Co-authored-by: Phil Bennett <pbennett@neworbit.co.uk>

---
+++ packages/tap-bark/src/components/failure-detail.spec.tsx
@@ -0,0 +1,24 @@
+
+
+import * as React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+import { Test, TestFixture, Expect, TestCase } from "../../../alsatian";
+import { FailureDetail } from "./failure-detail";
+
+@TestFixture("<FailureDetails /> Tests")
+export class FailureDetailTests {
+    @TestCase(null)
+    @TestCase(undefined)
+    @TestCase({})
+    @TestCase({ phil: "rob", dawid: "binks" })
+    @Test("weird inputs")
+    public weirdThingsDoesntBreak(value: any) {
+        const result = shallow(<FailureDetail name="something" value={value} />);
+
+        Expect(() => result.html()).not.toThrow();
+    }
+}
